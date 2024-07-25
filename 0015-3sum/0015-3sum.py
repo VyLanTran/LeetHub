e52e95a3ed6d5915464fe185c5d11c8a1312ff7a
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-07-25 14:21:24 -0400

    Time: 1572 ms (34.28%), Space: 15 MB (67.56%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,47 +5,47 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        res, n = set(), len(nums)
+        # res, n = set(), len(nums)
+        # nums.sort()
+        # for i in range(n):
+        #     j, k = i + 1, n - 1
+        #     while j < k:
+        #         sum = nums[i] + nums[j] + nums[k]
+        #         if sum == 0:
+        #             res.add((nums[i], nums[j], nums[k]))
+        #             j += 1
+        #             k -= 1
+        #         elif sum < 0:
+        #             j += 1
+        #         else:
+        #             k -= 1
+        # return res
+        
+        res, n = [], len(nums)
         nums.sort()
         for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             j, k = i + 1, n - 1
             while j < k:
                 sum = nums[i] + nums[j] + nums[k]
                 if sum == 0:
-                    res.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                    k -= 1
+                    res.append([nums[i], nums[j], nums[k]])
+                    cur = nums[j]
+                    while j < n and nums[j] == cur:
+                        j += 1
+                    cur = nums[k]
+                    while k >= 0 and nums[k] == cur:
+                        k -= 1
                 elif sum < 0:
-                    j += 1
+                    cur = nums[j]
+                    while j < n and nums[j] == cur:
+                        j += 1
                 else:
-                    k -= 1
+                    cur = nums[k]
+                    while k >= 0 and nums[k] == cur:
+                        k -= 1
         return res
-        
-#         res, n = [], len(nums)
-#         nums.sort()
-#         for i in range(n):
-#             if i > 0 and nums[i] == nums[i - 1]:
-#                 continue
-#             j, k = i + 1, n - 1
-#             while j < k:
-#                 sum = nums[i] + nums[j] + nums[k]
-#                 if sum == 0:
-#                     res.append([nums[i], nums[j], nums[k]])
-#                     cur = nums[j]
-#                     while j < n and nums[j] == cur:
-#                         j += 1
-#                     cur = nums[k]
-#                     while k >= 0 and nums[k] == cur:
-#                         k -= 1
-#                 elif sum < 0:
-#                     cur = nums[j]
-#                     while j < n and nums[j] == cur:
-#                         j += 1
-#                 else:
-#                     cur = nums[k]
-#                     while k >= 0 and nums[k] == cur:
-#                         k -= 1
-#         return res
 
             
     
