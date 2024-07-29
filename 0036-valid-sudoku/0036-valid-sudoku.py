22fcccc1e8087d57f596ea220dfc123666c0a577
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-07-29 14:57:33 -0400

    Time: 97 ms (28.51%), Space: 16.6 MB (39.70%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -9,22 +9,12 @@ class Solution:
             for j in range(n):
                 k = (i // 3) * 3 + (j // 3)
                 val = board[i][j]
-                # print(val)
-                # if val == "8":
-                #     print(val, rows[i], cols[j], squares[k])
-                #     continue
                 if val == ".":
-                    # print(i, j)
                     continue
                 if val in rows[i] or val in cols[j] or val in squares[k]:
                     return False
                 rows[i].add(val)
                 cols[j].add(val)
                 squares[k].add(val)
-                # print("here")
                 
         return True
-    
-    '''
-["8","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]
-    '''
