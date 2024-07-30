2d47523f8251c6c79f1111c939713e802ffe5366
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-07-30 11:28:21 -0400

    Time: 43 ms (6.17%), Space: 16.4 MB (52.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,5 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        '''
-        1, 2, 3, 5
-        
-        '''
         if n <= 2:
             return n
         prev, cur = 1, 2
