0a3d070e9a87393fbbae7d5e54c5a00510dbed38
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-07-31 11:50:59 -0400

    Time: 56 ms (73.67%), Space: 16.7 MB (51.29%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,23 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        def countFreq(s):
+            freq = dict()
+            for c in s:
+                freq[c] = freq.get(c, 0) + 1
+            return freq
         
-        def countFreq(word):
-            map = dict()
-            for c in word:
-                map[c] = 1 + (map[c] if c in map else 0)
-            return map
+        freq = countFreq(s1)
         
-        map1 = countFreq(s1)
-        map2 = dict()
         i = 0
         for j in range(len(s2)):
             c = s2[j]
-            map2[c] = 1 + (map2[c] if c in map2 else 0)
-            freq = map1[c] if c in map1 else 0
-            while map2[c] > freq:
-                map2[s2[i]] -= 1
+            freq[c] = freq.get(c, 0) - 1
+            while freq[c] < 0:
+                freq[s2[i]] += 1
                 i += 1
             if j - i + 1 == len(s1):
                 return True
         return False
+        
             
+            
+
+       
+        
