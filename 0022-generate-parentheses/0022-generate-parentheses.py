199a2b6572ac0153f318096b08782b25fe56485c
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-07-31 13:27:21 -0400

    Time: 43 ms (22.85%), Space: 16.7 MB (67.26%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,37 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        dp = dict()
+#         dp = dict()
         
-        def rec(n, open):
-            if (n, open) in dp:
-                return dp[(n, open)]
-            if n == 0:
-                return [")" * open]
-            val = set()
-            arr1 = rec(n - 1, open + 1)
-            for s in arr1:
-                val.add("(" + s)
-            if open > 0:
-                arr2 = rec(n, open - 1)
-                for s in arr2:
-                    val.add(")" + s)
-            dp[(n, open)] = val
-            return val
+#         def rec(n, open):
+#             if (n, open) in dp:
+#                 return dp[(n, open)]
+#             if n == 0:
+#                 return [")" * open]
+#             val = set()
+#             arr1 = rec(n - 1, open + 1)
+#             for s in arr1:
+#                 val.add("(" + s)
+#             if open > 0:
+#                 arr2 = rec(n, open - 1)
+#                 for s in arr2:
+#                     val.add(")" + s)
+#             dp[(n, open)] = val
+#             return val
             
-        return rec(n, 0)
+#         return rec(n, 0)
+
+        res = []
+    
+        def rec(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return
+            if left < n:
+                rec(left + 1, right, s + "(")
+            if right < left:
+                rec(left, right + 1, s + ")")
+        rec(0, 0, "")
+        return res
+            
+            
