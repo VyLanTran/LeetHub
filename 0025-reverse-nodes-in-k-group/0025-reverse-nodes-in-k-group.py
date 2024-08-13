6a8c690a95ac7a7bb699f82d036e99373a67a23f
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-08-14 00:32:06 +0700

    Time: 45 ms (43.23%), Space: 17.7 MB (8.28%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -6,32 +6,7 @@
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
-        '''
-        0, 1, 2, 3, 4, 5
-        1, 2, 3, 4, 5, 6
-        i  i  i  i  i  i  i
-        p        p
-        c  c  c  c  c  c  c
-       
-        
-        [0]
-        
-        [1]
-        '''
-        
         def reverse(head):
-            '''
-       <-  1  2
-            a  b    c        
-        while b.next:
-        b.next = a
-        a = b
-        b = c
-        c = b.next
-        
-        ====
-        b.next = a
-            '''
             if not head or not head.next:
                 return head
             a, b, c = None, head, head.next
@@ -56,17 +31,11 @@ class Solution:
         
         while fast:
             if fastIndex % k == 0:
-                # print("slowIndex:", slowIndex, "fastIndex:", fastIndex)
                 if slowIndex is not None:
                     heads.append(slow)
-                    # if slowIndex == 0:
-                    #     print("arr should have pointer 1:", heads)
                 slowIndex = fastIndex
                 slow = fast
                 fast = fast.next
-                
-                # if fastIndex == 0:
-                #     print("slow pt should be 1:", slow)
             elif fastIndex % k == k - 1:
                 temp = fast.next
                 fast.next = None
@@ -76,16 +45,8 @@ class Solution:
                 
             fastIndex += 1
             
-        # print("fastIndex:", fastIndex, "slow:", slow)
         if fastIndex % k == 0:
             heads.append(slow)
-            
-        '''
-        [1, 3]
-        '''
-        
-        # print("heads", heads)
-        # print("extra:", extraHead)
         
         dummy = ListNode(0)
         cur = dummy
