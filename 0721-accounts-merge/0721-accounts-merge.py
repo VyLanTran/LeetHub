7732646e8cbe6c2eb3c46684116ebb0ee20b8c77
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-08-17 04:44:03 +0700

    Time: 187 ms (34.79%), Space: 21.9 MB (30.22%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,6 +1,51 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+
+        numAccounts = len(accounts)
+        visitedAccounts = [False for _ in range(numAccounts)]
+        emailAccountMap = dict()
+        res = []
+
+        def buildGraph():
+            for id, account in enumerate(accounts):
+                for i in range(1, len(account)):
+                    email = account[i]
+                    if email not in emailAccountMap:
+                        emailAccountMap[email] = []
+                    emailAccountMap[email].append(id)
+                    
+        buildGraph()
         
+        def dfs(id, emails):
+            if visitedAccounts[id]:
+                return
+            visitedAccounts[id] = True
+            for i in range(1, len(accounts[id])):
+                email = accounts[id][i]
+                emails.add(email)
+                if email in emailAccountMap:
+                    for id2 in emailAccountMap[email]:
+                        dfs(id2, emails)
+        
+        for id, account in enumerate(accounts):
+            if visitedAccounts[id]:
+                continue
+            # print(id)
+            emails = set()
+            name = account[0]
+            dfs(id, emails)
+            emails = list(emails)
+            emails.sort()
+            # print(emails)
+            arr = [name]
+            arr.extend(emails)
+            res.append(arr)
+        
+        return res
+            
+
+
+        '''
         def createGraph():
             graph = dict()
             emailToName = dict()
@@ -53,6 +98,7 @@ class Solution:
             res.append(arr)
             
         return res
+        '''
             
             
             
