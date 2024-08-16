6549a9d36e0c31ada0fda27804a3d0ca64871ae9
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-08-17 06:10:30 +0700

    Time: 35 ms (72.97%), Space: 16.5 MB (28.28%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -6,27 +6,36 @@
 #         self.right = right
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
-        queue = deque()
-        queue.append(root)
-        isLevelFull, lastNodeNextLevel = True, False
-        level = 0
+        bfs = [root]
+        i = 0
+        while bfs[i]:
+            bfs.append(bfs[i].left)
+            bfs.append(bfs[i].right)
+            i += 1
+        return not any(bfs[i:])
+    
+#         queue = deque()
+#         queue.append(root)
+#         isLevelFull, lastNodeNextLevel = True, False
+#         level = 0
         
-        while queue:
-            if not isLevelFull:
-                return False
-            isLevelFull = len(queue) == pow(2, level)
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                if lastNodeNextLevel and (node.left or node.right):
-                    return False
-                if not node.left and node.right:
-                    return False
-                if not node.left or not node.right:
-                    lastNodeNextLevel = True
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            level += 1
-        return True
+#         while queue:
+#             if not isLevelFull:
+#                 return False
+#             isLevelFull = len(queue) == pow(2, level)
+#             for _ in range(len(queue)):
+#                 node = queue.popleft()
+#                 if lastNodeNextLevel and (node.left or node.right):
+#                     return False
+#                 if not node.left and node.right:
+#                     return False
+#                 if not node.left or not node.right:
+#                     lastNodeNextLevel = True
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             level += 1
+#         return True
+            
             
