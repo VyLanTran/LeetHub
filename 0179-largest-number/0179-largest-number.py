ec7a75136a19b29069dc64cc22ef38e93bc5b946
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-09-18 10:22:59 +0700

    Time: 43 ms (43.32%), Space: 16.4 MB (74.36%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -6,17 +6,18 @@ class Solution:
         def compare(num1, num2):
             return 1 if num1 + num2 >= num2 + num1 else -1
         
-        def removeLeadingZeros(s):
-            i = 0
-            while i < len(s) and s[i] == '0':
-                i += 1
-            if i >= len(s):
-                return "0"
-            return s[i:len(s)]
+        # def removeLeadingZeros(s):
+        #     i = 0
+        #     while i < len(s) and s[i] == '0':
+        #         i += 1
+        #     if i >= len(s):
+        #         return "0"
+        #     return s[i:len(s)]
         
         nums = [str(num) for num in nums]
         nums.sort(key=cmp_to_key(compare), reverse=True)
-        return removeLeadingZeros(''.join(nums))
+        # return removeLeadingZeros(''.join(nums))
+        return "0" if nums[0] == "0" else ''.join(nums)
         
         
                  
