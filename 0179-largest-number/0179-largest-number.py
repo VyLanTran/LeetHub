5ee64c84ff8e667fab0fbd82beabd1fa8c9300a2
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-09-18 10:21:38 +0700

    Time: 39 ms (68.62%), Space: 16.5 MB (74.36%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,10 +1,22 @@
+from functools import cmp_to_key
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        def compare(a, b): 
-            if a + b > b + a:
-                return -1
-            return 1
+        
+        def compare(num1, num2):
+            return 1 if num1 + num2 >= num2 + num1 else -1
+        
+        def removeLeadingZeros(s):
+            i = 0
+            while i < len(s) and s[i] == '0':
+                i += 1
+            if i >= len(s):
+                return "0"
+            return s[i:len(s)]
         
         nums = [str(num) for num in nums]
-        nums = sorted(nums, key = cmp_to_key(compare))
-        return "0" if nums[0] == "0" else "".join(nums)
+        nums.sort(key=cmp_to_key(compare), reverse=True)
+        return removeLeadingZeros(''.join(nums))
+        
+        
+                 
