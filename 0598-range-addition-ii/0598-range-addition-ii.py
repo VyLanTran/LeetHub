13e2858a8a71228815fb181be404ebee9a1070e6
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-09-19 11:21:18 +0700

    Time: 69 ms (46.25%), Space: 18.5 MB (94.67%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.py
+++ 0598-range-addition-ii/0598-range-addition-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        minRow, minCol = m, n
         for row, col in ops:
-            minRow = min(minRow, row)
-            minCol = min(minCol, col)
-        return minRow*minCol
+            m = min(m, row)
+            n = min(n, col)
+        return m*n
