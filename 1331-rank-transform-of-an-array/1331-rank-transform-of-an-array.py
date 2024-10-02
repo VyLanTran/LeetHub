f4a3abe9f908afb75292c497ebe99bdabe48e078
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-10-02 17:37:35 +0700

    Time: 315 ms (6.31%), Space: 33.3 MB (75.10%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,12 +1,15 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        arr = [[arr[i], i] for i in range(len(arr))]
-        arr.sort(key=lambda x:x[0])
-        res = [0 for _ in range(len(arr))]
-        rank, curVal = 0, float('-inf')
-        for val, index in arr:
+        minHeap = copy.deepcopy(arr)
+        heapify(minHeap)
+        valToRank, rank, curVal = {}, 0, float('-inf')
+        while minHeap:
+            val = heappop(minHeap)
             if val > curVal:
                 rank += 1
-            res[index] = rank
+            valToRank[val] = rank
             curVal = val
-        return res
+        arr = [valToRank[val] for val in arr]
+        return arr
+        
+            
