82327139d4cfd0a8a5f06b31ec42642764e05cba
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-10-05 11:14:09 +0700

    Time: 50 ms (87.68%), Space: 16.7 MB (22.10%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,26 +1,27 @@
 class Solution:
+    '''
+    ab"
+    01234567
+    eidboaoo
+    iiiii
+    jjjjjj
+    
+    a: 0, b: 0
+    e: 0
+    '''
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        def countFreq(s):
-            freq = dict()
-            for c in s:
-                freq[c] = freq.get(c, 0) + 1
-            return freq
-        
-        freq = countFreq(s1)
-        
+        freq = [0 for _ in range(26)]
         i = 0
+        for char in s1:
+            index = ord(char) - ord('a')
+            freq[index] += 1
         for j in range(len(s2)):
-            c = s2[j]
-            freq[c] = freq.get(c, 0) - 1
-            while freq[c] < 0:
-                freq[s2[i]] += 1
+            index = ord(s2[j]) - ord('a')
+            freq[index] -= 1
+            while freq[index] < 0:
+                freq[ord(s2[i]) - ord('a')] += 1
                 i += 1
             if j - i + 1 == len(s1):
                 return True
         return False
-        
-            
-            
-
-       
-        
+                
