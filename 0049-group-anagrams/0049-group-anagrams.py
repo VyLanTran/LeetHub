f5df6b4f2f7b19e1a99e89744295f588360927ba
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-10-31 17:39:11 -0700

    Time: 6 ms (98.32%), Space: 19.6 MB (80.10%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        def countFrequency(word):
-            freq = [0] * 26
-            for char in word:
-                freq[ord(char) - ord('a')] += 1
-            return tuple(freq)
+       
         
         anagrams = defaultdict(list)
         for word in strs:
-            pattern = countFrequency(word)
+            pattern = ''.join(sorted(word))
             anagrams[pattern].append(word)
-        res = []
-        for arr in anagrams.values():
-            res.append(arr)
-        return res
+        return list(anagrams.values())
