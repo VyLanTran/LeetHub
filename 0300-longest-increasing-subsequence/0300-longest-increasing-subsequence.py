4679d4eccad749641a4b2c4006cb4b74f2c5bbef
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-11-14 14:35:30 +0700

    Time: 1 ms (97.94%), Space: 17 MB (20.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,20 +1,33 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        '''
-        0 ,1,2,3,4,5,6  ,7
-        10,9,2,5,3,7,101,18
-        1. 1 1 2 2 3. 4. 4
-        '''
-        
         n = len(nums)
-        dp = [1] * n
-        res = 1
-        for i in range(n):
-            maxLen = 1
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    maxLen = max(maxLen, 1 + dp[j])
-            dp[i] = maxLen
-            res = max(res, maxLen)
-        return res
+        incr_sequence = []
+        
+        def findFirstLarger(target):
+            left, right = 0, len(incr_sequence) - 1
+            res = 0
+            while left <= right:
+                mid = left + (right - left) // 2
+                if incr_sequence[mid] > target:
+                    res = mid
+                    right = mid - 1
+                elif incr_sequence[mid] == target:
+                    return -1
+                else:
+                    left = mid + 1
+            return res
+        
+        for num in nums:
+            if len(incr_sequence) == 0 or num > incr_sequence[-1]:
+                incr_sequence.append(num)
+            else:
+                # find first num that is < num
+                # print("before:", incr_sequence)
+                index = findFirstLarger(num)
+                if index != -1:
+                    incr_sequence[index] = num
+        #         print("after:", incr_sequence)
+        # print(incr_sequence)
+        return len(incr_sequence)
+                
         
