6186827c5616655b34f4566a61494954f21d3a11
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-11-14 07:00:42 +0700

    Time: 125 ms (43.55%), Space: 16.5 MB (72.69%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,43 +1,24 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:     
-        
-        def helper(s, left, right):
-            count = 0
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
-            return count
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        def countFromMiddle(i):
+            oddCount, evenCount = 0, 0
+            
+            for j in range(n):
+                if i - j < 0 or i + j >= n or s[i - j] != s[i + j]:
+                    break
+                oddCount += 1
+                
+            j = i + 1
+            if j < n:
+                for k in range(n):
+                    if i - k < 0 or j + k >= n or s[i - k] != s[j + k]:
+                        break
+                    evenCount += 1
+                    
+            return oddCount + evenCount
         
         res = 0
-        for mid in range(len(s)):
-            res += helper(s, mid, mid)
-            res += helper(s, mid, mid + 1)
-        
+        for i in range(n):
+            res += countFromMiddle(i)
         return res
-                
-        
-#         def countOddLen(s):
-#             count = 0
-#             for i in range(len(s)):
-#                 for k in range(0, len(s)):
-#                     if i - k < 0 or i + k >= len(s) or s[i - k] != s[i+ k]:
-#                         break
-#                     count += 1
-#             return count
-        
-#         def countEvenLen(s):
-#             count = 0
-#             for i in range(1, len(s)):
-#                 center = i - 0.5
-#                 dist = 0.5
-#                 while center - dist >= 0 and center + dist < len(s):
-#                     if s[int(center - dist)] == s[int(center + dist)]:
-#                         count += 1
-#                         dist += 1
-#                     else:
-#                         break
-#             return count
-        
-#         return countOddLen(s) + countEvenLen(s)
-                        
