93f056f1a056b2dcf63d8beeb081d3302009ecde
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-17 10:25:41 -0700

    Time: 11 ms (72.39%), Space: 17.9 MB (50.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,37 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        a: 3
+        n: 1
+        g: 1
+        r: 1
+        m: 1
+
+        - if not same length --> false
+        - find letter frequency for s
+        - loop through t
+            - if freq in s = 0 --> False
+            - else, -1
+        
+
+        test cases:
+        1. not same length -> F
+
+        Time: O(m + n), m = len(s), n = len(t)
+        Space: O(1)
+        '''
+
         if len(s) != len(t):
             return False
-        count = [0] * 26
-        for c in s:
-            count[ord(c) - ord('a')] += 1
-        for c in t:
-            index = ord(c) - ord('a')
-            count[index] -= 1
-            if count[index] < 0:
+        letter_freq = [0 for _ in range(26)]
+
+        for letter in s:
+            letter_freq[ord(letter) - ord('a')] += 1
+
+        for letter in t:
+            index = ord(letter) - ord('a')
+            if letter_freq[index] == 0:
                 return False
+            letter_freq[index] -= 1
+
         return True
