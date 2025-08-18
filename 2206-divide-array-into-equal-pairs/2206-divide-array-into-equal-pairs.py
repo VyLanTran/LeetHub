137d0dfb773202da543471d8948912e766c6ced1
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-18 07:58:03 -0700

    Time: 4 ms (38.96%), Space: 17.9 MB (42.6%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,12 +1,4 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        num_freq = defaultdict(int)
-
-        for num in nums:
-            num_freq[num] += 1
-
-        for val in num_freq.values():
-            if val % 2 == 1:
-                return False
-
-        return True
+        num_freq = Counter(nums)
+        return all(freq % 2 == 0 for freq in num_freq.values())
