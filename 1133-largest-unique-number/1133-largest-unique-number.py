e280dc5290381438c0c5212c68dd743c8e673847
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-19 23:28:58 -0700

    Time: 3 ms (28.92%), Space: 17.9 MB (87.23%) - LeetHub

---
+++ 1133-largest-unique-number/1133-largest-unique-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        '''
+        5,7,3,9,4,9,8,3,1
+        '''
+
+        freq = defaultdict(int)
+        max_num = -1
+
+        for num in nums:
+            freq[num] += 1
+
+        for key, val in freq.items():
+            if val == 1:
+                max_num = max(max_num, key)
+
+        return max_num
