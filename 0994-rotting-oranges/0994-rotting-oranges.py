d3949f9727cca434ddf4b0c13330d048c66667e5
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-23 13:25:03 -0700

    Time: 1 ms (89.6%), Space: 17.9 MB (44.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,48 +1,49 @@
-from collections import deque
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        dirs = [[-1,0],[1,0],[0,-1],[0,1]]
-        
-        def isValid(i, j):
-            return i >= 0 and i < rows and j >= 0 and j < cols
-        
-        def initiateState():
-            rotten_oranges = deque()
-            fresh_oranges = 0
-            for i in range(rows):
-                for j in range(cols):
-                    val = grid[i][j]
-                    if val == 0:
-                        continue
-                    elif val == 2:
-                        rotten_oranges.append((i, j)) 
-                    else:
-                        fresh_oranges += 1
-            return fresh_oranges, rotten_oranges
-        
-        fresh_oranges, rotten_oranges = initiateState()
-        if fresh_oranges == 0:
+        '''
+        2 1 1 
+        0 1 1 
+        1 0 1
+
+        - first, go through the matrix to
+            - count number of fresh oranges
+            - add the coordinate of all rotten oranges into a queue
+        - bfs until queue becomes empty
+            - decrement number of fresh oranges
+            - increment number of minutes after each iteration
+        - return -1 if fresh ornages still exist, else num_minutes
+        '''
+
+        minutes = -1
+        fresh_count = 0
+        queue = deque()
+        rows, cols = len(grid), len(grid[0])
+        direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    fresh_count += 1
+                elif grid[i][j] == 2:
+                    queue.append((i, j))
+                else:
+                    pass
+
+        if fresh_count == 0:
             return 0
-        time = -1
-        while rotten_oranges:
-            time += 1
-            size = len(rotten_oranges)
+
+        while queue:
+            size = len(queue)
             for _ in range(size):
-                i, j = rotten_oranges.popleft()
-                for di, dj in dirs:
-                    newI = i + di
-                    newJ = j + dj
-                    if isValid(newI, newJ) and grid[newI][newJ] == 1:
-                        fresh_oranges -= 1
-                        rotten_oranges.append((newI, newJ))
-                        grid[newI][newJ] = 2
-        return time if fresh_oranges == 0 else -1
-    
-    '''
-    2 1 1
-    0 1 1
-    1 0 1
-    '''
+                i, j = queue.popleft()
+                for di, dj in direction:
+                    new_i = i + di
+                    new_j = j + dj
+                    if new_i >= 0 and new_i < rows and new_j >= 0 and new_j < cols and grid[new_i][new_j] == 1:
+                        grid[new_i][new_j] = 2
+                        fresh_count -= 1
+                        queue.append((new_i, new_j))
+            minutes += 1
+
+        return minutes if fresh_count == 0 else -1
+
