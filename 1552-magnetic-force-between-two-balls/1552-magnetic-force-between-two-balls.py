dab6ad37655e7a180ab032bf1a1db88672c1ace9
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-24 16:55:32 -0700

    Time: 239 ms (95.9%), Space: 30.3 MB (82.9%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -25,6 +25,14 @@ class Solution:
         if m == 2:
             return position[-1] - position[0]
 
+        '''
+        right = (position[-1] - position[0]) // (m - 1) 
+        this is a stricter bound to optimize binary search
+        this formula is how we calculate maximum min force between 2
+        consecutive balls - which happens supposed (ideally) that the 
+        baskets are evenly distributed, ie, any 2 consecutive baskets
+        maintain a fixed distance = total distance between first and last basets / number of spaces in between, ie, m - 1
+        '''
         left, right = 1, (position[-1] - position[0]) // (m - 1)
         max_force = 1
         while left <= right:
