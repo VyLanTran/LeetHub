2f9d476fcd1fe8da8eb87cd9f482636e1f37ef26
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-23 18:06:41 -0700

    Time: 7 ms (81.34%), Space: 17.9 MB (83.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,33 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         '''
-        - len == 1 => True
-        - #@#@ => True
+        - no alphanumeric -> True
+
+        left, right
+        while left <= right:
+            if left char is not alphanumeric:
+                left += 1
+            elif right char is not alphanumeric:
+                right -= 1
+            else:
+                convert both into lowercase
+                if not match:
+                    return False
+                left += 1
+                right -= 1
         '''
 
-        i, j = 0, len(s) - 1
+        left, right = 0, len(s) - 1
 
-        while i < j:
-            leftChar, rightChar = s[i], s[j]
-            if not leftChar.isalnum():
-                i += 1
-            elif not rightChar.isalnum():
-                j -= 1
-            elif leftChar.lower() != rightChar.lower():
+        while left <= right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
                 return False
             else:
-                i += 1
-                j -= 1
-
-        return True
+                left += 1
+                right -= 1
         
-
+        return True
