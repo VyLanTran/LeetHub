3365ad82c38867a2b7c02628873fc842d311a1e0
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-25 19:10:06 -0700

    Time: 0 ms (100%), Space: 17.8 MB (60.4%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -0,0 +1,13 @@
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        if len(set(suits)) == 1:
+            return "Flush"
+        
+        counter = Counter(ranks)
+        max_freq = max(counter.values())
+
+        if max_freq >= 3:
+            return "Three of a Kind"
+        if max_freq == 2:
+            return "Pair"
+        return "High Card"
