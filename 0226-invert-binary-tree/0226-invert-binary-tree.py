93952dbffbc8ff67ddcbae23a947d473fcf8608a
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-27 21:09:27 -0400

    Time: 0 ms (100%), Space: 17.5 MB (96.88%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -6,11 +6,16 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        '''
+        Time: O(n), n = number of nodes in the tree
+        Space: O(n), in the worse case, tree is a string-shape thus the 
+        stack size is O(n)
+        '''
         if not root:
             return root
         self.invertTree(root.left)
         self.invertTree(root.right)
         root.left, root.right = root.right, root.left
-        
+
         return root
             
