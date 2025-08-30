85acc3744c0cf7e8b15cbad4fd75326b6493b5a7
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-30 16:28:35 -0400

    Time: 23 ms (18.05%), Space: 22.6 MB (25.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-       
-        
-        anagrams = defaultdict(list)
-        for word in strs:
-            pattern = ''.join(sorted(word))
-            anagrams[pattern].append(word)
-        return list(anagrams.values())
+        '''
+        n = number of strings
+        m = max len of a string
+
+        Time: O(mn)
+        Space: O(n)
+        '''
+
+        def encode(s):
+            letter_freq = [0 for _ in range(26)]
+            for char in s:
+                letter_freq[ord(char) - ord('a')] += 1
+            return tuple(letter_freq)
+
+        anagram_map = defaultdict(list)
+
+        for s in strs:
+            anagram_map[encode(s)].append(s)
+
+        return list(anagram_map.values())
