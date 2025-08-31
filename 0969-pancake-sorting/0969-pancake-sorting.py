bf4e73fdbddc6a8b5e786485cb6ed433009a670f
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-31 16:24:59 -0400

    Time: 5 ms (23.35%), Space: 17.7 MB (93.01%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,7 +1,8 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         '''
-        3,2,4,1
+        Time: O(n^2)
+        Space: O(1)
 
         after a flip arr[0:i], we get
             [i, i-1, i-2, ..., 1, 0, i+1, i+2, ..., n-1]
@@ -22,14 +23,13 @@ class Solution:
         -  -
         1, 2, 3, 4
 
-        ===
-        0 1 2 3 4  
-        4 3 2 1 0
+ 
         '''
 
         n = len(arr)
         res = []
 
+        # Time: O(n)
         def flip(start, end):
             i, j = start, end
             while i < j:
@@ -37,6 +37,7 @@ class Solution:
                 i += 1
                 j -= 1
 
+        # Time: O(n)
         def find_max_index(start, end):
             max_num_index = start
             for i in range(start + 1, end + 1):
@@ -46,9 +47,10 @@ class Solution:
 
         for i in range(n - 1, 0, -1):
             target = find_max_index(0, i)
-            res.append(target + 1)
-            flip(0, target)
-            res.append(i + 1)
-            flip(0, i)
+            if target != i:
+                res.append(target + 1)
+                flip(0, target)
+                res.append(i + 1)
+                flip(0, i)
 
         return res
