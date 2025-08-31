559b78a5d65a7503b66fb5e791f96b8e31c6a104
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-31 01:25:35 -0400

    Time: 687 ms (60.48%), Space: 75.9 MB (26.75%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -1,26 +1,10 @@
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         '''
-        max sum = sum of 2 largest num
-        min sum = sum of 2 smallest num
-        what are all powers of 2 in the range
-            int(log(min_sum)) <= power <= int(log(max_sum))
-            -> list out all 2 ** power
-
-        res = 0, 1, 2, 3, 4
-        range: 1 -> 5
-        sums = [2, 4, 8, 16, 32]
-        (3, 1)
-        (5, 3)
-        (7, 1)
-        (9, 7)
-        {
-            1: 1
-            3: 1
-            5: 1
-            7: 1
-            9: 1
-        }
+        n = len of array
+        k = max value in array
+        Time: O(nlog(k)) 
+        Space: O(log(k) + n) = O(n) - this is worst case, when all value are distinct
         '''
 
         MOD = 10 ** 9 + 7
