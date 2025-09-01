4453d3e6ef7bc0bff67443e8903d9f730143b4d7
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-31 22:54:03 -0400

    Time: 232 ms (86.67%), Space: 20.1 MB (68.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,12 +1,11 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_islands = 0
         rows, cols = len(grid), len(grid[0])
+        res = 0
 
         def dfs(i, j):
-            if not (0 <= i < rows and 0 <= j < cols) or grid[i][j] != "1":
+            if not (0 <= i < rows and 0 <= j < cols and grid[i][j] == "1"):
                 return
-            # mark as visited
             grid[i][j] = "#"
             dfs(i - 1, j)
             dfs(i + 1, j)
@@ -17,5 +16,6 @@ class Solution:
             for j in range(cols):
                 if grid[i][j] == "1":
                     dfs(i, j)
-                    num_islands += 1
-        return num_islands
+                    res += 1
+
+        return res
