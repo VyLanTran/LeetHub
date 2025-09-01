c4df626bdc1d4333964739b59ad6bd9bd0106f97
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-08-31 21:41:23 -0400

    Time: 53 ms (85.94%), Space: 22.4 MB (76.57%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,22 +1,21 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        maxHeap = []
-        
-        def calSquDist(point):
-            return pow(point[0], 2) + pow(point[1], 2)
-        
-        for point in points:
-            if len(maxHeap) < k:
-                heappush(maxHeap, (-calSquDist(point), point))
+        '''
+        n = number of points in array
+        Time: O(nlog(k))
+        Space: O(k)
+        '''
+
+        max_heap = []
+
+        for i, point in enumerate(points):
+            x, y = point
+            dist_squared = x ** 2 + y ** 2
+            if len(max_heap) >= k and dist_squared >= -max_heap[0][0]:
                 continue
-            dist = calSquDist(point)
-            if dist >= -maxHeap[0][0]:
-                continue
-            heappop(maxHeap)
-            heappush(maxHeap, (-dist, point))
-            
-        res = []
-        while maxHeap:
-            res.append(heappop(maxHeap)[1])
-        return res
-            
+            if len(max_heap) >= k:
+                heappop(max_heap)
+            heappush(max_heap, (-dist_squared, i))
+
+        return [points[tup[1]] for tup in max_heap]
+        
