8261586f939988348e38611dddaf483811299407
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-02 23:55:55 -0400

    Time: 131 ms (60.19%), Space: 30.7 MB (24.46%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -24,15 +24,24 @@ class Solution:
         score = first num + last num + sum(consec pairs of weight we selected)
 
 
-        0, 1, |2, |3, 4, 5
+        1 4 2 5 2
+        5 6 7 7
 
-        if k = 1
-        just sum first and last num
-
+        0 1 2 3 4 5 6 7 8 -> n = 9
+        want to get d numbers => 9-d to 9
         '''
 
         if k == 1:
             return 0
         
         consec_pair_sums = [weights[i] + weights[i + 1] for i in range(len(weights) - 1)]
-        return max(consec_pair_sums) - min(consec_pair_sums)
+        
+        # for max score, choose the k-1 max val in consec_pair_sums 
+        # opposite for min score 
+        consec_pair_sums.sort()
+
+        n = len(consec_pair_sums)
+        # print(consec_pair_sums)
+        # print(n - 1, n - (k-1))
+
+        return sum([consec_pair_sums[i] for i in range(n - 1, n - (k-1) - 1, -1)]) - sum([consec_pair_sums[i] for i in range(k-1)])
