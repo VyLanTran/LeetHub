96f6eb02e3c1fee214ec9144757c47ef4b6899cf
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-03 00:48:56 -0400

    Time: 322 ms (82.07%), Space: 22 MB (54.02%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -1,12 +1,19 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
         '''
-        
+        n = number of mechanics
+        M = (largest rank) * cars^2
+
+        Time: 
+            O(nlog(M) + nlog(n)) if sorting ranks 
+
+        Space: O(1)
         '''
 
         # optional
         # ranks.sort()
 
+        # O(n)
         def is_possible(target):
             cars_done = 0
             for rank in ranks:
@@ -15,7 +22,8 @@ class Solution:
                     return True
             return False
 
-        low, high = 1, ranks[-1] * (cars ** 2)
+        # low, high = 1, ranks[-1] * (cars ** 2) # if ranks was sorted
+        low, high = 1, max(ranks) * (cars ** 2)
         best_time = high
 
         while low <= high:
