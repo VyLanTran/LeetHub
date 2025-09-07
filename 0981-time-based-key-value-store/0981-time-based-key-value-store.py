5a8d2784aa1bd752337fa4d1c91d5ef45028a505
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-07 17:51:51 -0400

    Time: 140 ms (34.57%), Space: 73.8 MB (85.96%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,54 +1,29 @@
-# class TimeMap:
-
-#     def __init__(self):
-#         self.map = dict()        
-
-#     def set(self, key: str, value: str, timestamp: int) -> None:
-#         arr = self.map.get(key, [])
-#         arr.append((timestamp, value))
-#         self.map[key] = arr
-
-#     def get(self, key: str, timestamp: int) -> str:
-#         if key not in self.map:
-#             return ""
-#         arr = self.map[key]
-#         i, j = 0, len(arr) - 1
-#         while i <= j:
-#             mid = i + (j - i) // 2
-#             tup = arr[mid]
-#             time, val = tup[0], tup[1]
-#             if time > timestamp:
-#                 j = mid - 1
-#             elif mid == len(arr) - 1 or (mid + 1 < len(arr) and arr[mid + 1][0] > timestamp):
-#                 return val
-#             else:
-#                 i = mid + 1
-#         return ""
-
-class TimeMap(object):
+class TimeMap:
 
     def __init__(self):
-        self.dic = collections.defaultdict(list)
+        self.map = defaultdict(list)
         
 
-    def set(self, key, value, timestamp):
-        self.dic[key].append([timestamp, value])
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append((timestamp, value))
+        
 
-    def get(self, key, timestamp):
-        arr = self.dic[key]
-        n = len(arr)
-        
-        left = 0
-        right = n
-        
-        while left < right:
-            mid = (left + right) // 2
-            if arr[mid][0] <= timestamp:
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.map[key]
+        left, right = 0, len(arr) - 1
+        res = ""
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            time, val = arr[mid]
+            if time <= timestamp:
+                res = val
                 left = mid + 1
-            elif arr[mid][0] > timestamp:
-                right = mid
+            else:
+                right = mid - 1
         
-        return "" if right == 0 else arr[right - 1][1]
+        return res
+
         
 
 
@@ -56,3 +31,9 @@ class TimeMap(object):
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+'''
+{
+    foo: [(1, bar)]
+}
+'''
