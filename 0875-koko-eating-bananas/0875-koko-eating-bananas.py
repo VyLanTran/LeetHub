79a1712c7d25a2c189d696b134ccd4540ef2d838
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-07 13:24:57 -0400

    Time: 159 ms (51.93%), Space: 19.1 MB (32.46%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,27 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         '''
-        [3, 6, 7, 11]
-        
+        k = max pile
+        Time: O(nlog(k))
+        Space: O(1)
         '''
-        
+
         left, right = 1, max(piles)
-        
-        def canFinish(k):
-            totalTime = 0
-            for pile in piles:
-                totalTime += ceil(pile / k)
-                if totalTime > h:
-                    return False
-            return True
-        
-        res = right
+        min_speed = right
+
+        def is_possible(speed):
+            return sum(ceil(pile / speed) for pile in piles) <= h
+
         while left <= right:
             mid = left + (right - left) // 2
-            if canFinish(mid):
-                res = mid
+            if is_possible(mid):
+                min_speed = mid
                 right = mid - 1
             else:
                 left = mid + 1
-        return res
-                
+        
+        return min_speed
