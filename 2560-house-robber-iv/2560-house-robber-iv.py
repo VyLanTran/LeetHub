89f167d78fa2d55cb1ee78e6f58b6e360ca6adc9
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-07 22:56:11 -0400

    Time: 237 ms (87.46%), Space: 32.1 MB (5.06%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        '''
+        targets = [1, 2, 3, 7, 9]
+
+        is_possible(target):
+            # is it possible to store from >= k houses st 
+            # max money <= target
+        '''
+
+        targets = set(nums)
+        targets = sorted(list(targets))
+        
+        def is_possible(target):
+            i = 0
+            houses_robbed = 0
+            while i < len(nums):
+                if nums[i] <= target:
+                    houses_robbed += 1
+                    if houses_robbed >= k:
+                        return True
+                    i += 2
+                else:
+                    i += 1
+            return False
+
+        left, right = 0, len(targets) - 1
+        min_index = right
+        while left <= right:
+            mid = left + (right - left) // 2
+            if is_possible(targets[mid]):
+                min_index = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return targets[min_index]
