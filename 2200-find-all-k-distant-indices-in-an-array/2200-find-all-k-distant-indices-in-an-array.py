e04666de340466b5e186015622c1c191cfdfca64
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-08 23:58:45 -0400

    Time: 3 ms (82.96%), Space: 17.9 MB (66.18%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.py
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.py
@@ -5,14 +5,31 @@ class Solution:
         Space: O(n)
         '''
 
-        target_indices = [i for i, num in enumerate(nums) if num == key]
-        prev_right = -1
+        # target_indices = [i for i, num in enumerate(nums) if num == key]
+        # prev_right = -1
+        # res = []
+
+        # for i in target_indices:
+        #     left = max(prev_right + 1, i - k)
+        #     right = min(len(nums) - 1, i + k)
+        #     # res.extend([j for j in range(left, right + 1)])
+        #     prev_right = right
+        
+        # return res
+
+        '''
+        Time = Space = O(n)
+        '''
+
+        prev_right = 0 # the min index that is not yet judged
         res = []
 
-        for i in target_indices:
-            left = max(prev_right + 1, i - k)
-            right = min(len(nums) - 1, i + k)
-            res.extend([j for j in range(left, right + 1)])
-            prev_right = right
-        
+        for i, num in enumerate(nums):
+            if num == key:
+                start = max(prev_right, i - k)
+                end = min(len(nums) - 1, i + k)
+                for j in range(start, end + 1):
+                    res.append(j)
+                prev_right = end + 1
+
         return res
