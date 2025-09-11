01eef0c0d4e669cd1382eadac25e0801344038b0
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-10 23:26:16 -0400

    Time: 0 ms (100%), Space: 17.8 MB (61.54%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        '''
+        
+        '''
+
+        word_len, abbr_len = len(word), len(abbr)
+        i, j = 0, 0
+
+        while i < word_len and j < abbr_len:
+            abbr_char = abbr[j]
+            if abbr_char.isalpha():
+                if abbr_char != word[i]:
+                    return False
+                i += 1
+                j += 1
+            elif abbr_char == '0':
+                return False
+            else:
+                num_string = ""
+                while j < abbr_len and abbr[j].isnumeric():
+                    num_string += abbr[j]
+                    j += 1
+                i += int(num_string)
+        
+        return i == word_len and j == abbr_len
