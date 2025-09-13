e3359af694a4f2e9ce857222a2ad28c1ae77d563
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-13 14:55:02 -0400

    Time: 1138 ms (18.47%), Space: 43.5 MB (13.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,49 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         '''
-        [1, 2, 5]
-        amount = 11
-        
-        amounts = 
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-        [0, 1, 1, 2, 3, 1, _, 2, 3, _ , _ , _]
-        '''
-        if amount == 0:
-            return 0
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
-        for i in range(len(dp)):
-            for coin in coins:
-                if i + coin < amount + 1:
-                    dp[i + coin] = min(dp[i + coin], 1 + dp[i])
+        4, 5
 
-        return dp[-1] if dp[-1] != float('inf') else -1
+        8 = 4 + 4
+        
+        sort coin values first
+        f(amount) = what is the fewest number of coins to make up this amount
+            if amount < 0:
+                return inf
+            if amount == 0:
+                return 0
+            if amount in cache:
+                return from cache
+
+            min_count = inf
+            for coin (from largest to smallest):
+                num_coins = f(amount - value)
+                min_count = min(min_count, 1 + num_coins)
+            cache[amount] = min_count if min_count != inf
+
+
+        f(11)
+            f(6)
+                f(1) = 1
+                    f(-4) = inf
+                    f(-1) = inf
+                    f(0) = 0
+        '''
+
+        cache = {}
+        coins.sort(reverse=True)
+
+        def rec(amount):
+            if amount < 0:
+                return float('inf')
+            if amount == 0:
+                return 0
+            if amount in cache:
+                return cache[amount]
+            res = float('inf')
+            for coin in coins:
+                res = min(res, 1 + rec(amount - coin))
+            cache[amount] = res
+            return res
+
+        res = rec(amount)
+        return res if res != float('inf') else -1
