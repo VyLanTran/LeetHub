537af2115d2f881f868440149a1ef9754b598673
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-13 11:22:59 -0400

    Time: 5426 ms (19%), Space: 18.1 MB (22.02%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols, n = len(board), len(board[0]), len(word)
-        
-        def rec(i, j, k):
-            if k >= n:
+        rows, cols = len(board), len(board[0])
+        cache = {}
+
+        def dfs(r, c, i):
+            if i >= len(word):
                 return True
-            if i < 0 or i >= rows or j < 0 or j >= cols or board[i][j] != word[k]:
+            if not (0 <= r < rows and 0 <= c < cols):
                 return False
-            board[i][j] = "*"
-            res = rec(i - 1, j, k + 1) or rec(i + 1, j, k + 1) or rec(i, j - 1, k + 1) or rec(i, j + 1, k + 1)
-            board[i][j] = word[k]
+            if (r, c) in cache: 
+                return cache[(r, c, i)]
+
+            if board[r][c] != word[i]:
+                cache[(r, c, i)] = False
+                return False
+
+            visited_char = board[r][c]
+            board[r][c] = "*"
+            res = dfs(r - 1, c, i + 1) or dfs(r + 1, c, i + 1) or dfs(r, c - 1, i + 1) or dfs(r, c + 1, i + 1)
+            cache[(r, c, i)] = res
+            board[r][c] = visited_char
             return res
-        
-        for i in range(rows):
-            for j in range(cols):
-                if rec(i, j, 0):
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
                     return True
+
         return False
-            
-            
+
