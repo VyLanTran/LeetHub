714f5081f41aab77be1dbd10696bcf6e7ac93d8c
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-13 17:16:14 -0400

    Time: 298 ms (60.17%), Space: 17.7 MB (96.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,34 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         '''
-        0123
-        cbbd
+        0 + 1 + 2 + ... + 2 + 0 + 1
+        0 -> n/2
+
+        Time: O(n^2)
+        Space: O(1)
         '''
-        n = len(s)
+        s_len = len(s)
+        best_start, best_end = 0, -1
+
+        def expand_palindrome(start, end):
+            while start - 1 >= 0 and end + 1 < s_len and s[start - 1] == s[end + 1]:
+                start -= 1
+                end += 1
+            return start, end
+
+        for i in range(s_len):
+            start, end = expand_palindrome(i, i)
+            if end - start > best_end - best_start:
+                best_start, best_end = start, end
+            if i < s_len - 1 and s[i] == s[i + 1]:
+                start, end = expand_palindrome(i, i + 1)
+            if end - start > best_end - best_start:
+                best_start, best_end = start, end
         
-        def helper(i):
-            odd = s[i]
-            even = ""
-            
-            for j in range(1, n):
-                if i - j < 0 or i + j >= n:
-                    break
-                if s[i - j] != s[i + j]:
-                    break
-                odd = s[i - j] + odd + s[i + j]
-            
-            j = i + 1
-            if j < n:
-                for k in range(n):
-                    if i - k < 0 or j + k >= n:
-                        break
-                    if s[i - k] != s[j + k]:
-                        break
-                    even = s[i - k] + even + s[j + k]
-            return odd if len(odd) >= len(even) else even
+        return s[best_start:(best_end + 1)]
+
+
         
-        res = ""
-        for i in range(n):
-            candidate = helper(i)
-            res = candidate if len(candidate) > len(res) else res
-        return res
