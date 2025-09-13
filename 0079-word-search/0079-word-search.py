b18c361d6d539033e977b81e9c2e906dd8bcb86a
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-13 11:27:10 -0400

    Time: 3228 ms (77.85%), Space: 17.8 MB (87.74%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,23 +3,23 @@ class Solution:
         '''
         '''
         rows, cols = len(board), len(board[0])
-        cache = {}
+        # cache = {}
 
         def dfs(r, c, i):
             if i >= len(word):
                 return True
             if not (0 <= r < rows and 0 <= c < cols):
                 return False
-            if (r, c) in cache: 
-                return cache[(r, c, i)]
+            # if (r, c) in cache: 
+            #     return cache[(r, c, i)]
 
             if board[r][c] != word[i]:
-                cache[(r, c, i)] = False
+                # cache[(r, c, i)] = False
                 return False
 
             board[r][c] = "*"
             res = dfs(r - 1, c, i + 1) or dfs(r + 1, c, i + 1) or dfs(r, c - 1, i + 1) or dfs(r, c + 1, i + 1)
-            cache[(r, c, i)] = res
+            # cache[(r, c, i)] = res
             board[r][c] = word[i]
             return res
 
