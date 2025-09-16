8625ba98f5da814cede9aa179f519e768881aad2
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-16 16:08:37 -0400

    Time: 155 ms (92%), Space: 45.3 MB (78%) - LeetHub

---
+++ 3004-maximum-subtree-of-the-same-color/3004-maximum-subtree-of-the-same-color.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maximumSubtreeSize(self, edges: List[List[int]], colors: List[int]) -> int:
+        '''
+                0,1
+            1,2       2,1
+
+        '''
+        max_count = 1
+
+        def create_graph():
+            graph = defaultdict(list)
+            for u, v in edges:
+                graph[u].append((v, colors[v]))
+            return graph
+
+        graph = create_graph()
+
+        def dfs(node):
+            if node not in graph:
+                return 1, True
+
+            nonlocal max_count
+            color = colors[node]
+            is_valid_tree = True
+            tree_size = 1
+            
+            for child, child_color in graph[node]:
+                subtree_size, is_valid_subtree = dfs(child)
+                is_valid_tree = is_valid_tree and child_color == color and is_valid_subtree
+                tree_size += subtree_size
+
+            if is_valid_tree:
+                max_count = max(max_count, tree_size)
+            
+            return tree_size, is_valid_tree
+
+        dfs(0)
+
+        return max_count
