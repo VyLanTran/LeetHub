f2fd32245492bb7c433cb36ec42f7e7ae5efa5b2
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-09-20 17:28:55 -0400

    Time: 0 ms (100%), Space: 17.6 MB (86.69%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,33 +1,20 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        '''
-        2
-        4
-        16
-        37
-        52
-        29
-        85  64 + 25
-        89  64 + 81
-        145 25 16 1
-        42
-        20
-        4
-        '''
-        def sumSqrtDigits(n):
+        visited = set()
+
+        def find_squared_sum(n):
             res = 0
             while n > 0:
-                res += pow(n%10, 2)
-                n = n // 10
+                res += (n % 10) ** 2
+                n //= 10
             return res
-            
-        seenNums = set()
+
         while True:
-            n = sumSqrtDigits(n)
             if n == 1:
                 return True
-            if n in seenNums:
+            if n in visited:
                 return False
-            seenNums.add(n)
-    
+            visited.add(n)
+            n = find_squared_sum(n)
+
             
