38f491b566584bd0f7353dd1bfbd0be180040385
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-09 09:11:29 -0400

    Time: 111 ms (67.18%), Space: 20.8 MB (55.16%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,18 +6,8 @@ class Solution(object):
         """
 
         '''
-        0,1,2,3,4,5,6,7,8
-        1,8,6,2,5,4,8,3,7
-        i
-                        j
-
-        V = (i - j) * min(h[i], h[j])
-
-        if h[i] < h[j]
-            i += 1
-        if h[i] > h[j]:
-            j = 1
-        if equal
+        Time: O(n)
+        Space: O(1)
         '''
 
         max_area = 0
