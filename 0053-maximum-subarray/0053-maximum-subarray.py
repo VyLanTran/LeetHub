b2ac726a428619d3a6c34f1898f41f172af23a34
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-11 13:55:55 -0400

    Time: 103 ms (8.89%), Space: 32.9 MB (22.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,27 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        maxLocal, maxGlobal = nums[0], nums[0]
-        
-        for i in range(1, len(nums)):
-            maxLocal = max(nums[i], nums[i] + maxLocal)
-            maxGlobal = max(maxGlobal, maxLocal)
-        
-        return maxGlobal
+        '''
+         0,1, 2,3, 4,5,6, 7,8
+        -2,1,-3,4,-1,2,1,-5,4
+
+        max_global_sum
+        for each index i, want to find
+            sum[i] = max sum of a subarray that ends with nums[i]
+            update max_sum
+
+        sum[i]
+            sum[i - 1] + nums[i]
+            nums[i]
+        keep track of prevoius max_local_sum = sum[i - 1]
+        '''
+
+        nums_len = len(nums)
+        max_global_sum = nums[0]
+        max_local_sum = 0
+
+        for i, num in enumerate(nums):
+            max_local_sum = max(max_local_sum + num, num)
+            max_global_sum = max(max_global_sum, max_local_sum)
+
+        return max_global_sum
+
