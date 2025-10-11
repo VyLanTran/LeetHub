23d765a7f3a82045113d5a737878a1510ecd4f26
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-11 15:34:35 -0400

    Time: 4 ms (1.14%), Space: 18 MB (8.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-        prev, cur = 1, 2
-        for i in range(3, n + 1):
-            next = prev + cur
-            prev, cur = cur, next
-        return cur
+        '''
+        Why dp[i] = dp[i - 1] + dp[i - 2]
+        To reach dp[i], there are 2 ways
+            1. Take dp[i - 2] ways + the last time is a 2-step
+            2. Take dp[i - 1] ways + the last time is a 1-step
+        => Finbonacci
+        '''
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        prev1, prev2 = 1, 2
+
+        for i in range(n - 2):
+            prev1, prev2 = prev2, prev1 + prev2
+
+        return prev2
