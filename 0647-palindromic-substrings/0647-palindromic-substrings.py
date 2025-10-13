7d059bed8f56aa1074878d789862929ac7419e8f
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-13 16:25:07 -0400

    Time: 137 ms (41.37%), Space: 18 MB (34.12%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,24 +1,17 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        def countFromMiddle(i):
-            oddCount, evenCount = 0, 0
-            
-            for j in range(n):
-                if i - j < 0 or i + j >= n or s[i - j] != s[i + j]:
-                    break
-                oddCount += 1
-                
-            j = i + 1
-            if j < n:
-                for k in range(n):
-                    if i - k < 0 or j + k >= n or s[i - k] != s[j + k]:
-                        break
-                    evenCount += 1
-                    
-            return oddCount + evenCount
-        
+
+        def extend_palindrome(a, b):
+            i, j = a, b
+            while i - 1 >= 0 and j + 1 < n and s[i - 1] == s[j + 1]:
+                i -= 1
+                j += 1
+            return a - i + 1
+
         res = 0
         for i in range(n):
-            res += countFromMiddle(i)
+            res += extend_palindrome(i, i)
+            if i + 1 < n and s[i] == s[i + 1]:
+                res += extend_palindrome(i, i + 1)
         return res
