62bb82ebc4e81e0001230601923380a33107f6fa
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-13 18:10:48 -0400

    Time: 251 ms (8.44%), Space: 39.1 MB (51.19%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.py
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.py
@@ -1,40 +1,8 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
         '''
-        after choosing the first 2 numbers, we can decide the rest greedily
-        o, o => the rest must be odd
-        e, e => the rest must be even
-        o, e => the rest alternating
-        e, o => the rest alternating
-
-        first 2 pairs must be 1 of 4 cases
-
-        0,1,2,3,4,5,6
-        1,2,1,1,2,1,2
-
-        # f(i) = max len if a valid subsequence if we choose from [0:i] inclusively
-
-        f(i, prev1, prev2):
-            if i >= n:
-                return 1
-            
-            if both prevs are not Null => we can decide whether to include i or not
-            if we can add i:
-                return 1 + f(i + 1, prev2, nums[i])
-            else:
-                return f(i + 1, prev1, prev2)
-            
-            if both are None:
-                # if we choose current num
-                return 1 + f(i + 1, nums[i], prev2)
-                # if skip
-                return f(i + 1, prev1, prev2)
-            else: (ie, prev1 is not None but prev2 is None)
-                # if select
-                return 1 + f(i + 1, prev, nums[i])
-                # if skip:
-                return f(i + 1, prev1, prev2)
-
+        Time: O(n)
+        Space: O(1)
         '''
         n = len(nums)
 
@@ -47,9 +15,7 @@ class Solution:
                     length += 1
             return length
 
-        # want first event => is_event1 = True
-        # 0, 1, 2
-        # 1, 2, 1
+  
         def find_max_len(is_even1, is_even2):
             i = 0
             
