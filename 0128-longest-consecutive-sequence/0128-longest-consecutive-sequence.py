2248b88ae0ca824a8cef948b1b4bb5775af42241
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-14 11:49:07 -0400

    Time: 125 ms (8.62%), Space: 82.5 MB (5.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,17 +1,26 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        uniqueNums = set(nums)
-        dp = dict()
-        longestStreak = 0
-        
-        def rec(num):
+        '''
+        100,4,200,1,3,2
+          1,
+        '''
+
+        unique_nums = set(nums)
+        n = len(nums)
+        dp = {}
+        max_len = 0
+
+        def f(num):
             if num in dp:
                 return dp[num]
-            res = 1 + (rec(num - 1) if num - 1 in uniqueNums else 0)
+            if num - 1 not in unique_nums:
+                dp[num] = 1
+                return 1
+            res = 1 + f(num - 1)
             dp[num] = res
             return res
-        
-        for num in nums:
-            longestStreak = max(longestStreak, rec(num))
-        return longestStreak
-        
+
+        for num in unique_nums:
+            max_len = max(max_len, f(num))
+
+        return max_len
