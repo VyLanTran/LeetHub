c3fea7fb48f1b5c018b6d86b7f7fadbc1a6aabb0
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-15 10:59:12 -0400

    Time: 1 ms (93.31%), Space: 19.9 MB (51.17%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,16 +1,20 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        '''
+        n = len(intervals)
+        Time: O(nlog(n))
+        Space: O(n)
+        '''
+
         intervals.sort(key = lambda x:x[0])
-        endTime = []
-        res = 0
-        
-        for interval in intervals:
-            start, end = interval[0], interval[1]
-            if not endTime or (endTime and endTime[0] > start):
-                res += 1
-            else:
-                heappop(endTime)
-            heappush(endTime, end)
-            
-        return res
-        
+        required_rooms = 0
+        pending = []
+
+        for start, end in intervals:
+            while pending and pending[0] <= start:
+                heapq.heappop(pending)
+            heapq.heappush(pending, end)
+            required_rooms = max(required_rooms, len(pending))
+
+        return required_rooms
+
