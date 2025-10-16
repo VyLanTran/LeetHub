58f55b46956044847bd2ad836815d96dfe41941e
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-16 11:08:48 -0400

    Time: 1727 ms (40.4%), Space: 288.9 MB (22.38%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,23 +1,10 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         '''
-            0       1       2       3       4
-        [(1, 1), (3, 1), (2, 4), (0, 1), (1, 0)]
-
-        may sort the data in ascending order
-
-        f(i, m, n) = max size of subset if you can use any of s between [0, i]
-            if i < 0:
-                return 0
-            a, b = freq[i]
-            # can we skip early if know that the result can't beat max_size
-            if a > m or b > n:
-                # must skip
-                res = f(i - 1, m, n)
-                put into cache
-            # use substring i
-            res = max(1 + f(i - 1, m - a, n - b), f(i - 1, m, n))
-            put into cache
+        k = number of strings in strs
+        N = total number of char in all strings
+        Time: O(N + kmn)
+        Space: O(kmn)
         '''
 
         strs_len = len(strs)
