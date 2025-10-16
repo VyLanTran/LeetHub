b87fa28b3c3384b9b73639fa81e6c1edd54df4e9
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-16 12:00:51 -0400

    Time: 4246 ms (5.3%), Space: 18.3 MB (63.12%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -7,6 +7,7 @@ class Solution:
         Space: O(kmn)
         '''
 
+        """
         strs_len = len(strs)
         freq = []
         dp = {}
@@ -23,6 +24,7 @@ class Solution:
         for s in strs:
             freq.append(count_zeroes_ones(s))
 
+        # Top-down
         def f(i, m, n):
             if i < 0:
                 return 0
@@ -38,5 +40,38 @@ class Solution:
             return res
 
         return f(strs_len - 1, m, n)
+        """
 
+        strs_len = len(strs)
+        freq = []
+        rows, cols = m + 1, n + 1
+        prev = [[0 for _ in range(cols)] for _ in range(rows)]
+        cur = [[0 for _ in range(cols)] for _ in range(rows)]
 
+        def count_zeroes_ones(s):
+            zeroes, ones = 0, 0
+            for char in s:
+                if char == '0':
+                    zeroes += 1
+                else:
+                    ones += 1
+            return (zeroes, ones)
+
+        for s in strs:
+            freq.append(count_zeroes_ones(s))
+
+        # Bottom-up
+        for i in range(1, strs_len + 1):
+            actual_index = i - 1
+            zeroes, ones = freq[actual_index]
+            for r in range(0, rows):
+                for c in range(0, cols):
+                    if zeroes > r or ones > c:
+                        cur[r][c] = prev[r][c]
+                    else:
+                        cur[r][c] = max(1 + prev[r - zeroes][c - ones], prev[r][c])
+            prev = copy.deepcopy(cur)
+
+        return cur[-1][-1]
+
+      
