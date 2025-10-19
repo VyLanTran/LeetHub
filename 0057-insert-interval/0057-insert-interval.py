ae5e462ad7fd909fb98eba3ff990d28c7516b14c
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-18 21:29:09 -0400

    Time: 0 ms (100%), Space: 19.7 MB (60.46%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,18 +1,8 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         '''
-        [1,2],[3,5],[6,7],[8,10],[12,16]
-          i
-
-        merge [s1,e1] to [s2, e2]
-        if e1 < s2:
-            put [s1, e1] to res
-            continue to merge [s2, e2] to next intervals
-        elif e2 < s1:
-            put [s2, e2] to res
-            put remaining intervals (from [s1,e1] to the end) into res
-        else:
-            new_interval = [min(s1, s2), max(e1, e2)]
+        Time: O(n)
+        Space: O(1)
         '''
 
         n = len(intervals)
