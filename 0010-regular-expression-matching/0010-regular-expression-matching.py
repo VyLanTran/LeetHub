e331ff7fb3242afe7f051702b92f830e33325b9d
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-20 17:41:59 -0400

    Time: 0 ms (100%), Space: 17.8 MB (82.62%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,6 +1,9 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         '''
+        Time: O(mn)
+        Space: O(mn)
+        
         Test cases
 
         s = c
@@ -10,60 +13,6 @@ class Solution:
         s = bd
         p = .b*d
         True because .=b, b*="", d=d
-
-        ---
-        01
-        bd
-        ii
-        0123
-        .b*d
-        __jj
-
-        {
-
-        }
-
-        f(1, 3)
-            f(0, 2)
-                F or f(0, 0) = T
-                    f(-1, -1) = T
-                T or f(-1, 2) = T
-                    f(-1, 0) = F
-        ---
-
-        i, j points to s, p giving a, b
-        f(i, j) = is it possible that s[0,i] match p[0,j]
-
-        f(i, j)
-            if (i, j) in cache:
-                return that
-            if i < 0:
-                if j < 0:
-                    return True
-                if b == '*':
-                    return f(i, j-2)
-                return False
-            if j < 0:
-                return False
-            if b is letter:
-                if a != b:
-                    return False
-                return f(i-1, j-1)
-            if b == '.':
-                return f(i-1, j-1)
-            # b == '*'
-            prev_char = p[j-1]'
-            if prev_char != '.' and prev_char != a:
-                return f(i,j-2)
-            res = False
-            # 0 occurence
-            res = res or f(i, j-2)
-            # decide to match 1 occurence
-            res = res or f(i-1,j)
-            put res into cache
-            return res
-
-
         '''
         m, n = len(s), len(p)
         dp = {}
