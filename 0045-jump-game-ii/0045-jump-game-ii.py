fc128ab5cc211d4691cb424a3cb1572c5be3500a
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-20 19:38:22 -0400

    Time: 3 ms (96.93%), Space: 18.6 MB (74.8%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,51 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        '''
+        Time: O(n^2)
+        Space: O(n)
+        ---
+        f(i) = min number of jumps to go from index 0 to index i
+            if i == 0:
+                return 0
+            if i in dp:
+                return
+            res = inf
+            for j in range(0, i):
+                largest_steps = nums[j]
+                distance = i - j
+                if distance <= largest_steps:
+                    res = min(res, 1 + f(j))
+            put res into dp
+            return res
+        '''
+
+        # n = len(nums)
+        # dp = [float('inf')] * n
+        # dp[0] = 0
+
+        # for i in range(1, n):
+        #     for j in range(i):
+        #         if i - j <= nums[j]:
+        #             dp[i] = min(dp[i], 1 + dp[j])
+        # return dp[-1]
+
+        '''
+        BFS is better because we find the smallest number of steps
+        so it's kinda like layers 0 (i=0), 1 (all indices reachable from layer 0 in 1 step), 2, 3, ...
+
+        Time: O(n)
+        Space: O(1)
+        '''
+
+        n = len(nums)
+        l, r = 0, 0
+        res = 0
+
+        while r < n - 1:
+            new_l = r + 1
+            new_r = r + 1
+            for i in range(l, r + 1):
+                new_r = max(new_r, i + nums[i])
+            l, r = new_l, new_r
+            res += 1
+        return res
