dbaa7b3592fbf797bed1cedf4ef7fc55fbee52ce
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 01:31:14 -0400

    Time: 1 ms (31.5%), Space: 18 MB (11.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,22 +1,35 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         '''
-        0 1
-        3 1 1
-        2 1 2
-        1 1 3
-        
+        f(i, j) = res if right corner is at (i, j)
+            if i == 0 and j == 0:
+                return 1
+            if i < 0 or j < 0: 
+                return 0
+            # reach from top cell (if applicable)
+            f(i - 1, j)
+            # reach from left cell 
+            f(i, j - 1)
+            return sum of the 2 results above
+
+        dp[0][_] = 1
+        dp[_][0] = 1
+        only need 2 rows: prev, cur
+
+        1 1 1 1 1 1 1
+        1 2 3 4 5 6 7
+        1 3 6  _ _ _
+                0 1 2 3 4 5 6
+        prev = [1 3 6 4 5 6 7]
+        cur. = [1 1 1 1 1 1 1]
+
+        i = 1
         '''
-        
-        dp = [[None] * n for _ in range(m)]
-        for i in range(m):
-            dp[i][-1] = 1
-        for j in range(n):
-            dp[m - 1][j] = 1
-            
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if not dp[i][j]:
-                    dp[i][j] = dp[i][j + 1] + dp[i + 1][j]
-        
-        return dp[0][0]
+
+        prev = [1] * n
+
+        for i in range(1, m):
+            for j in range(1, n):
+                prev[j] += prev[j - 1]
+        return prev[-1]
+
