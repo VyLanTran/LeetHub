e59a53c42cce243070b4964142ad7231b2f58820
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 00:16:35 -0400

    Time: 72 ms (32.87%), Space: 21.3 MB (95.93%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -9,28 +9,47 @@ class Solution:
         Time: O(n)
         Space: O(1)
         '''
+        # n = len(nums)
+        # num_ones = 0
+        # res = 0
+        # num_deletes = 1
+        # has_deleted = False
+        # l = 0
+
+        # for num in nums:
+        #     if num == 0:
+        #         num_deletes -= 1
+        #         has_deleted = True
+        #         while num_deletes < 0:
+        #             removed_num = nums[l]
+        #             if removed_num == 0:
+        #                 num_deletes += 1
+        #             else:
+        #                 num_ones -= 1
+        #             l += 1
+        #     else:
+        #         num_ones += 1
+        #     res = max(res, num_ones)
+
+        # if not has_deleted:
+        #     return max(res - 1, 0)
+        # return res
+
         n = len(nums)
-        num_ones = 0
         res = 0
-        num_deletes = 1
-        has_deleted = False
+        num_zeroes = 0
         l = 0
 
-        for num in nums:
+        for r, num in enumerate(nums):
             if num == 0:
-                num_deletes -= 1
-                has_deleted = True
-                while num_deletes < 0:
-                    removed_num = nums[l]
-                    if removed_num == 0:
-                        num_deletes += 1
-                    else:
-                        num_ones -= 1
-                    l += 1
-            else:
-                num_ones += 1
-            res = max(res, num_ones)
+                num_zeroes += 1
+            while num_zeroes > 1:
+                removed_num = nums[l]
+                if removed_num == 0:
+                    num_zeroes -= 1
+                l += 1
+            res = max(res, r - l)
 
-        if not has_deleted:
-            return max(res - 1, 0)
         return res
+
+     
