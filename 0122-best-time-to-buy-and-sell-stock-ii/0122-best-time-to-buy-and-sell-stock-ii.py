f3f37cb0adf02d048123628ede4ea5c0e3c756f7
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 00:43:10 -0400

    Time: 3 ms (38.74%), Space: 18.6 MB (98.4%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -5,13 +5,19 @@ class Solution:
         Space: O(1)
         '''
 
-        res = 0
-        n = len(prices)
-        l, r = 0, 0
+        # res = 0
+        # n = len(prices)
+        # l, r = 0, 0
 
-        while r < n:
-            while r + 1 < n and prices[r + 1] >= prices[r]:
-                r += 1
-            res += prices[r] - prices[l]
-            l, r = r + 1, r + 1
+        # while r < n:
+        #     while r + 1 < n and prices[r + 1] >= prices[r]:
+        #         r += 1
+        #     res += prices[r] - prices[l]
+        #     l, r = r + 1, r + 1
+        # return res
+
+        res = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                res += prices[i] - prices[i - 1]
         return res
