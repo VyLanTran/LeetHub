72eb0da35af1e824a35f44e331a062b17202f652
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 11:51:28 -0400

    Time: 3 ms (87.39%), Space: 18.7 MB (92.13%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,33 +1,17 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         '''
-        f(i) = result for triangle[0, i]
-            if i == 0:
-                return triangle[0][0]
-            if on row i, we take number at index j, then the previous cell leading
-            to this cell is either [i-1][j] or [i-1][j-1] if j-1 >= 0
-            dp[i][0] = triangle[i][0] + dp[i-1][0]
-            for j in range(1, end of cur row):
-                dp[i][j] = min(dp[i][j], cur_row[j] + dp[i-1][j], cur_row[j] + dp[i-1][j-1])
-
-        prev = [top value]
-
-        for i in range(1, last_row + 1):
-            cur_row
-            cur = [cur_row[0] + prev[0]]
-            for j in range(1, end of cur_row):
-                cur.append(cur_row[j] + min(prev[j - 1], prev[j]))
-            prev = cur_row
+        Time: 1 + 2 + ... + n = O(n^2) where n = triangle len
+        Space: O(n)
         '''
 
         num_rows = len(triangle)
-        # if num_rows == 1:
-        #     return triangle[0][0]
 
         prev = [triangle[0][0]]
 
         for i in range(1, num_rows):
             cur_row = triangle[i]
+            # a new object is assigned to cur here, so won't override prev
             cur = [cur_row[0] + prev[0]]
             for j in range(1, len(cur_row) - 1):
                 cur.append(cur_row[j] + min(prev[j - 1], prev[j]))
