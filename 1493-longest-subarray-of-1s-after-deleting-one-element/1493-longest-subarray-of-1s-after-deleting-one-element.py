87f1f530a1dc9d8f1caebe78d012533877ae19c1
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 00:06:45 -0400

    Time: 60 ms (60.54%), Space: 21.5 MB (78.92%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,35 +6,8 @@ class Solution:
         [1] -> 0
         [0, 0, 1] -> 1
 
-        num_deletes = 1, 0, -1, 0, -1, 0
-        res = 0, 1, 2, 3, 4, 5
-        num_ones = 0, 1, 2, 3, 4, 5, 4, 3, 2
-        has_deleted = False, T
-
-        0,1,2,3,4,5,6,7,8
-        0,1,1,1,0,1,1,0,1
-        r r r r r r r r r
-        l l l l l l
-
-        num_ones = 0
-        while r < n:
-            if num == 0:
-                num_deletes -= 1
-                has_deleted = True
-                while num_deletes < 0:
-                    removed_num = nums[l]
-                    if removed_num == 0:
-                        num_deletes += 1
-                    else:
-                        num_ones -= 1
-                    l += 1
-            else:
-                num_ones += 1
-            res = max(res, num_ones)
-            r += 1
-
-        if not has_deleted:
-            return max(res - 1, 0)
+        Time: O(n)
+        Space: O(1)
         '''
         n = len(nums)
         num_ones = 0
