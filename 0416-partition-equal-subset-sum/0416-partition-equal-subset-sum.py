17e22625131bfcb6f8d28a6ca37f831b4977b58d
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 02:28:13 -0400

    Time: 864 ms (44.05%), Space: 18 MB (74.6%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,51 +1,51 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         '''
-        sum odd => False
-        each has total = sum // 2
-        
-        
-        0,   1, 2, 3
-        [11, 5, 5, 1]
-        
-        f(i = 0, target = 11)
-            f(1, 0)
-         
-         0, 1, 2, 3
-        [6, 2, 8, 4], 10 each
-        
-        f(0, 10)
-            f(1, 4)
-                f(2, 2)
-                    f(3, 2)
-                        f(4, 2) X
-                    
-                
+        if sum not divisible by 2:
+            return False
+        target = sum // 2
+
+        f(i, target)
+            if target == 0:
+                return True
+            if in cache
+            return f(i - 1, target - num) or f(i - 1, target)
+
+        i: -1 to n - 1 => n + 1
+        j: 0 to target => target + 1
+        dp: matrix of size n x (target + 1)
+
+        dp[_][0] = True
+        dp[0][j] = False for any j != 0
+        don't matrix, just 
+
+        i but actual index is i - 1
+        prev = [False] * (target + 1) # stand for i = -1
+        prev[0] = True
+
+        for i from 0 -> n-1 (inclusively):
+            num = nums[i]
+            for j in from (target + 1) to 1 (inclu):
+                if j - num >= 0:
+                    prev[j] = prev[j] or (prev[j - num])
+
+
+        return prev[-1]
         '''
-        
+
+        n = len(nums)
         total = sum(nums)
         if total % 2 == 1:
             return False
         target = total // 2
+        prev = [False] * (target + 1)
+        prev[0] = True
+
+        for i in range(n):
+            num = nums[i]
+            for j in range(target, 0, -1):
+                if j - num >= 0:
+                    prev[j] = prev[j] or prev[j - num]
+
+        return prev[-1]
         
-        memo = {}
-        n = len(nums)
-        res = False
-        
-        def rec(i, target):
-            if i >= n:
-                return target == 0
-            if (i, target) in memo:
-                return memo[(i, target)]
-            # skip
-            if (nums[i] <= target and rec(i + 1, target - nums[i])) or rec(i + 1, target):
-                memo[(i, target)] = True
-                return True
-            memo[(i, target)] = False
-            return False
-        
-        
-        res =  rec(0, target)
-        # print(memo)
-        return res
-                
