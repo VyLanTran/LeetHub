b856c71ffe00674ba64234fbba67d0387f09d377
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-21 11:48:31 -0400

    Time: 3 ms (87.39%), Space: 18.7 MB (84.64%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,26 +1,38 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         '''
-        f(i, j, sum)
-            invalid if j out of bound
-            if i >= numRows: return sum 
-            min{f(i + 1, j - 1), f(i + 1, j + 1)}
+        f(i) = result for triangle[0, i]
+            if i == 0:
+                return triangle[0][0]
+            if on row i, we take number at index j, then the previous cell leading
+            to this cell is either [i-1][j] or [i-1][j-1] if j-1 >= 0
+            dp[i][0] = triangle[i][0] + dp[i-1][0]
+            for j in range(1, end of cur row):
+                dp[i][j] = min(dp[i][j], cur_row[j] + dp[i-1][j], cur_row[j] + dp[i-1][j-1])
+
+        prev = [top value]
+
+        for i in range(1, last_row + 1):
+            cur_row
+            cur = [cur_row[0] + prev[0]]
+            for j in range(1, end of cur_row):
+                cur.append(cur_row[j] + min(prev[j - 1], prev[j]))
+            prev = cur_row
         '''
-        
-        numRows = len(triangle)
-        res = float('inf')
-        dp = {}
-        
-        def rec(i, j):
-            if i >= numRows:
-                return 0
-            
-            if (i, j) in dp:
-                return dp[(i, j)]
-            res = triangle[i][j] + min(rec(i + 1, j), rec(i + 1, j + 1))
-            dp[(i, j)] = res
-            return res
-        
-        return rec(0, 0)
-        
-   
+
+        num_rows = len(triangle)
+        # if num_rows == 1:
+        #     return triangle[0][0]
+
+        prev = [triangle[0][0]]
+
+        for i in range(1, num_rows):
+            cur_row = triangle[i]
+            cur = [cur_row[0] + prev[0]]
+            for j in range(1, len(cur_row) - 1):
+                cur.append(cur_row[j] + min(prev[j - 1], prev[j]))
+            # the right most cell is only reachable from prev[j-1]
+            cur.append(cur_row[-1] + prev[-1])
+            prev = cur
+
+        return min(prev)
