e869b1073bd0f71a28b4f19f0a6e065bc684a8dd
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-24 20:13:32 -0400

    Time: 0 ms (100%), Space: 17.7 MB (88.41%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,26 +1,4 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        '''
-        1 => False
-        2 => True
-        3 => False
-        f(n) = can Alice win
-        f(n)
-            for x that is a valid divisor:
-                if not f(n - x):
-                    return True
-        {
-            1: F
-            2: T
-        }
-        '''
+        return n % 2 == 0
         
-        dp = [False] * (n + 1)
-        if n <= 1:
-            return dp[n]
-        for i in range(2, n + 1):
-            for j in range(1, i):
-                if i % j == 0 and not dp[i - j]:
-                    dp[i] = True
-                    break
-        return dp[-1]
