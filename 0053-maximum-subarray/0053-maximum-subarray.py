102fa1aa89a0208f583860a1b875e53bcb47953c
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-27 02:50:53 -0400

    Time: 91 ms (30.61%), Space: 32.9 MB (6.5%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,17 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        '''
-        Time: O(n)
-        Space: O(1)
-        '''
+        max_global = float('-inf')
+        max_local = 0
 
-        nums_len = len(nums)
-        max_global_sum = nums[0]
-        max_local_sum = 0
-
-        for i, num in enumerate(nums):
-            max_local_sum = max(max_local_sum + num, num)
-            max_global_sum = max(max_global_sum, max_local_sum)
-
-        return max_global_sum
-
+        for num in nums:
+            max_local = max(num, num + max_local)
+            max_global = max(max_global, max_local)
+        
+        return max_global
