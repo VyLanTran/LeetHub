f97f34c23b0be7ac3ba816cfe455ee2d186064e5
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-27 10:46:03 -0400

    Time: 4 ms (54.33%), Space: 19.9 MB (75.92%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,13 +1,25 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        required_rooms = 0
+        # required_rooms = 0
+        # pending_end = []
+        # intervals.sort()
+
+        # for start, end in intervals:
+        #     while len(pending_end) and pending_end[0] <= start:
+        #         heapq.heappop(pending_end)
+        #     heapq.heappush(pending_end, end)
+        #     required_rooms = max(required_rooms, len(pending_end))
+
+        # return required_rooms
+
         pending_end = []
         intervals.sort()
 
         for start, end in intervals:
-            while len(pending_end) and pending_end[0] <= start:
+            # we don't need to free up all possible rooms, just need to ask
+            # if we can free up 1 room to accommodate current meeting
+            if len(pending_end) > 0 and pending_end[0] <= start:
                 heapq.heappop(pending_end)
             heapq.heappush(pending_end, end)
-            required_rooms = max(required_rooms, len(pending_end))
 
-        return required_rooms
+        return len(pending_end)
