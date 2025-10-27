e35d068497707f13c4926518ce8017eb07d8dde5
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-27 10:30:08 -0400

    Time: 7 ms (36.3%), Space: 19.3 MB (5.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        tuples = [(nums[i], i) for i in range(len(nums))]
-        tuples.sort(key = lambda tup:tup[0])
-        left, right = 0, len(nums) - 1
-        
-        while left < right:
-            sum = tuples[left][0] + tuples[right][0]
-            if sum == target:
-                return [tuples[left][1], tuples[right][1]]
-            elif sum < target:
-                left += 1
+        n = len(nums)
+        arr = [(nums[i], i) for i in range(n)]
+        arr.sort()
+
+        i, j = 0, n - 1
+        while i < j:
+            num1, index1 = arr[i]
+            num2, index2 = arr[j]
+            total = num1 + num2
+            if total == target:
+                return [index1, index2]
+            elif total < target:
+                i += 1
             else:
-                right -= 1
-        
- 
+                j -= 1
