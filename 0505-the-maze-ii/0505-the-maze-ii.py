d448c4374231bf687a1722287cb139a9acb7853e
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-26 21:15:54 -0400

    Time: 68 ms (23.94%), Space: 18.5 MB (49.87%) - LeetHub

---
+++ 0505-the-maze-ii/0505-the-maze-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+        rows, cols = len(maze), len(maze[0])
+        start_i, start_j = start
+        dest_i, dest_j = destination
+        queue = deque([(start_i, start_j, 0)])
+        # visited = set([(start_i, start_j, 0)])
+        visited_dist = {
+            (start_i, start_j): 0
+        }
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        res = float('inf')
+
+        while len(queue) > 0:
+            i, j, dist = queue.popleft()
+            if i == dest_i and j == dest_j:
+                res = min(res, dist)
+            else:
+                for di, dj in directions:
+                    cur_i, cur_j = i, j
+                    while (0 <= cur_i < rows) and (0 <= cur_j < cols) and maze[cur_i][cur_j] == 0:
+                        cur_i += di
+                        cur_j += dj
+                    cur_i -= di
+                    cur_j -= dj
+                    cur_dist = abs(cur_i - i) + abs(cur_j - j) + dist
+                    # if (cur_i, cur_j, cur_dist) not in visited:
+                    if (cur_i, cur_j) not in visited_dist or visited_dist[(cur_i, cur_j)] > cur_dist:
+                        # visited.add((cur_i, cur_j, cur_dist))
+                        visited_dist[(cur_i, cur_j)] = cur_dist
+                        queue.append((cur_i, cur_j, cur_dist))
+        # print(visited_dist)
+        return -1 if res == float('inf') else res
