0ce7ae5bcc941fb209e60a67e3a26b8237c1ca0b
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-26 21:18:21 -0400

    Time: 51 ms (41.76%), Space: 18.6 MB (49.87%) - LeetHub

--- 0505-the-maze-ii/0505-the-maze-ii.py
+++ 0505-the-maze-ii/0505-the-maze-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+        '''
+        Time: O(m * n * max(m, n))
+        Space: O(m * n)
+        '''
         rows, cols = len(maze), len(maze[0])
         start_i, start_j = start
         dest_i, dest_j = destination
         queue = deque([(start_i, start_j, 0)])
-        # visited = set([(start_i, start_j, 0)])
         visited_dist = {
             (start_i, start_j): 0
         }
@@ -24,10 +27,7 @@ class Solution:
                     cur_i -= di
                     cur_j -= dj
                     cur_dist = abs(cur_i - i) + abs(cur_j - j) + dist
-                    # if (cur_i, cur_j, cur_dist) not in visited:
                     if (cur_i, cur_j) not in visited_dist or visited_dist[(cur_i, cur_j)] > cur_dist:
-                        # visited.add((cur_i, cur_j, cur_dist))
                         visited_dist[(cur_i, cur_j)] = cur_dist
                         queue.append((cur_i, cur_j, cur_dist))
-        # print(visited_dist)
         return -1 if res == float('inf') else res
