cdd916cdc2fa1ee17ad8ba2575d6ec0e6076df55
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-28 02:01:33 -0400

    Time: 46 ms (0.97%), Space: 17.6 MB (87.32%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,26 +1,33 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         '''
-        n = numRows
-        the length of the n-th row is n
+        if numRows <= 2:
+            special case
+        
+        res = [[1], [1, 1]]
+        for i from 2 to numRows: (i elements: 0, 1, ..., i - 1)
+            cur_row = [1]
+            for j from 1 to i - 2:
+                val = prev_row[j - 1] + prev_row[j]
+            cur_row.append(1)
 
-        Time: O(1 + 2 + 3 + .... + (n-1)) = O(n^2)
-        Space: O(1)
+        dp as prev
+
         '''
 
         if numRows == 1:
             return [[1]]
         if numRows == 2:
             return [[1], [1, 1]]
-
+        prev = [1, 1]
         res = [[1], [1, 1]]
 
-        for i in range(numRows - 2):
-            prev_row = res[-1]
-            cur_row = [1]
-            for j in range(len(prev_row) - 1):
-                cur_row.append(prev_row[j] + prev_row[j + 1])
-            cur_row.append(1)
-            res.append(cur_row)
-
+        for i in range(2, numRows):
+            cur = [1]
+            for j in range(1, i):
+                cur.append(prev[j- 1] + prev[j]) 
+            cur.append(1)
+            res.append(cur)
+            prev = cur
         return res
+
