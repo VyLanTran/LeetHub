852886c06f1ebab7524edf57f92763df6244d2fd
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-27 21:03:27 -0400

    Time: 3 ms (30.63%), Space: 18.2 MB (26.19%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,53 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
+        '''
         arr = []
+        number of opens/closes remaining to insert
+        always: num_open <= num_closes
 
-        def rec(open_count, close_count):
-            if open_count < 0 or close_count < 0:
+        f(num_open, num_close)
+            if num_open == 0:
+                turn arr into string, append ) * num_closes, then add to res
                 return
-            if open_count == 0 and close_count == 0:
-                res.append("".join(arr))
+            # always able to add an open
+            arr.append('(')
+            f(num_open - 1, num_close)
+            arr.pop()
+            if num_open < num_close:
+                arr.append(')')
+                f(num_open, num_close - 1)
+                arr.pop()
+
+        n = 2
+        ()(), (())
+        res = (())
+        arr = [(, )] 
+
+        f(2, 2)
+            f(1, 2)
+                f(0, 2) 
+                    "((" + "))" 
+                f(1, 1)
+
+                
+        '''
+
+        cur_parentheses = []
+        res = []
+
+        def f(num_open, num_close):
+            if num_open == 0:
+                s = "".join(cur_parentheses) + ")" * num_close
+                res.append(s)
                 return
-            if open_count > 0:
-                arr.append("(")
-                rec(open_count - 1, close_count)
-                arr.pop()
-            if close_count > open_count:
-                arr.append(")")
-                rec(open_count, close_count - 1)
-                arr.pop()
+            # always able to add an open
+            cur_parentheses.append('(')
+            f(num_open - 1, num_close)
+            cur_parentheses.pop()
+            if num_open < num_close:
+                cur_parentheses.append(')')
+                f(num_open, num_close - 1)
+                cur_parentheses.pop()
 
-        rec(n, n)
-
+        f(n, n)
         return res
