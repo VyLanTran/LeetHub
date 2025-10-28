ae3f759e53e803ef21a1076087beb3de31b639e6
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-27 20:10:26 -0400

    Time: 119 ms (34.49%), Space: 20.8 MB (54.93%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,20 +6,28 @@ class Solution(object):
         """
 
         '''
-        Time: O(n)
-        Space: O(1)
+        0,1,2,3,4,5,6,7,8
+        1,8,6,2,5,4,8,3,7
+                        r
+        l l
+        res = 8, 49
+
+        area = (r - l) * min(l, r)
+        if l < r:
+            # better to move l
+        else r > l:
+            # better to move r
         '''
-
         max_area = 0
-        left, right = 0, len(height) - 1
+        n = len(height)
+        left, right = 0, n - 1
 
-        while left <= right:
-            area = (right - left) * min(height[left], height[right])
+        while left < right:
+            left_height, right_height = height[left], height[right]
+            area = (right - left) * min(left_height, right_height)
             max_area = max(max_area, area)
-            if height[left] <= height[right]:
+            if left_height < right_height:
                 left += 1
             else:
                 right -= 1
-
         return max_area
-        
