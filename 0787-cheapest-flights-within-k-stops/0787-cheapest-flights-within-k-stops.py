746499987051428d00caa62d135190ecc66494e3
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-29 16:48:05 -0400

    Time: 0 ms (100%), Space: 19.3 MB (36.32%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        min_price = float('inf')
+        queue = deque([(src, 0)])
+        stops = 0
+        adj_list = defaultdict(list)
+        prices = [float('inf')] * n
+        prices[src] = 0
+
+        for from_city, to_city, price in flights:
+            adj_list[from_city].append((to_city, price))
+
+        while queue: 
+            size = len(queue)
+            for _ in range(size):
+                city, price = queue.popleft()
+                if city == dst:
+                    min_price = min(min_price, price)
+                else:
+                    for next_city, next_price in adj_list[city]:
+                        expected_price = price + next_price
+                        if prices[next_city] > expected_price:
+                            queue.append((next_city, price + next_price))
+                            prices[next_city] = expected_price
+            stops += 1
+            if stops > k + 1:
+                break
+
+        return -1 if min_price == float('inf') else min_price
