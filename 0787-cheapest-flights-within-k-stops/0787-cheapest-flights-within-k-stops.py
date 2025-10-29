3ca871e8cf181e722c752bca1463199281957746
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-29 18:08:36 -0400

    Time: 107 ms (11.83%), Space: 19.4 MB (26.39%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,26 +1,25 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # min_price = float('inf')
-        queue = deque([(src, 0)])
-        stops = 0
         adj_list = defaultdict(list)
-        prices = [float('inf')] * n
-        prices[src] = 0
+        prev = [float('inf')] * n
+        prev[src] = 0
 
         for from_city, to_city, price in flights:
-            adj_list[from_city].append((to_city, price))
+            adj_list[to_city].append((from_city, price))
 
-        while stops <= k and queue: 
-            size = len(queue)
-            for _ in range(size):
-                city, price = queue.popleft()
-              
-                for next_city, next_price in adj_list[city]:
-                    expected_price = price + next_price
-                    if prices[next_city] > expected_price:
-                        queue.append((next_city, price + next_price))
-                        prices[next_city] = expected_price
-            stops += 1
-           
+        print(adj_list)
 
-        return -1 if prices[dst] == float('inf') else prices[dst]
+        for i in range(k + 1):
+            cur = [float(inf)] * n
+            cur[src] = 0
+            for to_city in range(n):
+                for from_city, price in adj_list[to_city]:
+                    expected_price = price + prev[from_city]
+                    cur[to_city] = min(cur[to_city], expected_price)
+            print(prev)
+            print(cur)
+            print("--------")
+            prev = copy.deepcopy(cur)
+        return -1 if prev[dst] == float('inf') else prev[dst]
+
+
