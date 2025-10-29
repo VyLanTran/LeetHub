53e2c07cf73c382e9b4aedfd719fa81c3751ffaa
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-29 18:41:51 -0400

    Time: 0 ms (100%), Space: 17.9 MB (13.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         '''
-        Time: O(n)
-        Space: O(1)
+        dp[i] = max(dp[i - 1], num + dp[i - 2])
+        a -> i - 2
+        b -> i - 1
+
+        a = nums[0]
+        b = max(first 2 houses) only if n >= 2
+        i from 2 to n - 1
         '''
 
         n = len(nums)
+        a = nums[0]
         if n == 1:
-            return nums[0]
-        a, b = nums[0], max(nums[0], nums[1])
+            return a
+        b = max(a, nums[1])
 
         for i in range(2, n):
-            c = max(b, a + nums[i])
-            a, b = b, c
+            temp = max(b, a + nums[i])
+            a, b = b, temp
         
-        return max(a, b)
+        return b
