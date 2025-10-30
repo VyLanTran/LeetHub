04a47725c50edae756ddb7a5331d85d489727b0c
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-29 20:15:36 -0400

    Time: 389 ms (38.01%), Space: 19.6 MB (93.04%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,33 +1,31 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         
-        def buildAdjList(times):
-            adj = dict()
-            for time in times:
-                u, v, w = time[0] - 1, time[1] - 1, time[2]
-                arr = adj.get(u, [])
-                arr.append((v, w))
-                adj[u] = arr
-            return adj
-        
-        adj = buildAdjList(times)
-        
-        dist = [float('inf') for _ in range(n)]
-        dist[k - 1] = 0
-        queue = [(0, k - 1)]
-        
-        while queue:
-            pair = heappop(queue)
-            d, u = pair[0], pair[1]
-            if u not in adj:
-                continue
-            for (v, w) in adj[u]:
-                if dist[v] > d + w:
-                    dist[v] = d + w
-                    queue.append((dist[v], v))
-                    
-        res = max(dist)
-        return res if res != float('inf') else -1
-        
-        
-            
+        '''
+        1, 2, 3, 4
+        1, 0, 1 i
+
+        min_heap = (1, 1), (1, 3)
+
+        '''
+
+
+        adj_list = defaultdict(list)
+        min_times = {i: float('inf') for i in range(1, n + 1)}
+        min_times[k] = 0
+        min_heap = [(0, k)]
+
+        for u, v, w in times:
+            adj_list[u].append((v, w))
+
+        while min_heap:
+            time, u = heappop(min_heap)
+           
+            for v, travel_time in adj_list[u]:
+                expected_time = time + travel_time
+                if expected_time < min_times[v]:
+                    min_times[v] = expected_time
+                    heappush(min_heap, (expected_time, v))
+
+        max_time = max(min_times.values())
+        return -1 if max_time == float('inf') else max_time
