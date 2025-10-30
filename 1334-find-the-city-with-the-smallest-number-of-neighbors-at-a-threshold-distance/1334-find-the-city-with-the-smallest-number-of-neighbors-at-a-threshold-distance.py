0aaaec4336e6c65f5ecdb2e5bc734e38ad7c8c6f
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-29 21:01:08 -0400

    Time: 215 ms (60.44%), Space: 19.4 MB (19.04%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,45 +1,35 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        
-        def buildAdj():
-            adj = dict()
-            for [u, v, w] in edges:
-                arr1, arr2 = adj.get(u, []), adj.get(v, [])
-                arr1.append((v, w))
-                arr2.append((u, w))
-                adj[u] = arr1
-                adj[v] = arr2
-            return adj
-                
-        adj = buildAdj()
-        
-        def dijkstra(k):
-            dist = [float('inf') for _ in range(n)]
-            dist[k] = 0
-            queue = [(0, k)]
+        adj_list = defaultdict(list)
+        min_neighbors = float('inf')
+        res = 0
+
+        for u, v, w in edges:
+            adj_list[u].append((v, w))
+            adj_list[v].append((u, w))
+
+        def count_neighbors(src):
+            min_dist = [float('inf')] * n
+            min_dist[src] = 0
+            min_heap = [(0, src)]
             
-            while queue:
-                (d, u) = heappop(queue)
-                if u not in adj:
-                    continue
-                for (v, w) in adj[u]:
-                    if dist[v] > d + w:
-                        dist[v] = d + w
-                        queue.append((dist[v], v))
-                        
-            count = 0
-            for val in dist:
-                count += 1 if val <= distanceThreshold else 0
-            return count
-                    
-        minCity, res = n, 0
+            while min_heap:
+                dist, u = heappop(min_heap)
+                for v, w in adj_list[u]:
+                    expected_dist = dist + w
+                    if expected_dist < min_dist[v]:
+                        min_dist[v] = expected_dist
+                        heappush(min_heap, (expected_dist, v))
+            
+            return sum([1 if d <= distanceThreshold else 0 for d in min_dist])
+
+        
         for u in range(n):
-            count = dijkstra(u)
-            if count < minCity:
-                minCity = count
+            num_neighbors = count_neighbors(u)
+            if num_neighbors < min_neighbors:
+                min_neighbors = num_neighbors
                 res = u
-            elif count == minCity and u > res:
+            elif num_neighbors == min_neighbors and u > res:
                 res = u
+
         return res
-            
-                
