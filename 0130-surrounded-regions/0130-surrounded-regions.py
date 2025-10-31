682b6149f9692e036b2290a41984fc122011f045
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-10-31 00:23:39 -0400

    Time: 9 ms (32.92%), Space: 21.6 MB (88.23%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,27 +3,26 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        
         rows, cols = len(board), len(board[0])
-        
+
         def dfs(i, j):
-            if i < 0 or j < 0 or i >= rows or j >= cols or board[i][j] != "O": 
-                return 
+            if not (0 <= i < rows and 0 <= j < cols and board[i][j] == "O"):
+                return
             board[i][j] = "*"
             dfs(i - 1, j)
             dfs(i + 1, j)
             dfs(i, j - 1)
             dfs(i, j + 1)
-        
+
         for i in range(rows):
             for j in range(cols):
-                if (i == 0 or j == 0 or i == rows - 1 or j == cols - 1) and board[i][j] == "O":
+                if (i == 0 or i == rows - 1 or j == 0 or j == cols - 1) and board[i][j] == "O":
                     dfs(i, j)
         
         for i in range(rows):
             for j in range(cols):
-                board[i][j] = "O" if board[i][j] == "*" else "X"
-                
-        
-        
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "*":
+                    board[i][j] = "O"
             
