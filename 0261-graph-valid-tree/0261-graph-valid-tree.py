0c6d496a54cad0980ccfdb395a7d5217c0012db7
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-11-01 17:48:48 -0400

    Time: 0 ms (100%), Space: 19.3 MB (29.27%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,30 +1,31 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n == 1:
-            return True
-        def createGraph():
-            graph = defaultdict(list)
-            for i, j in edges:
-                graph[i].append(j)
-                graph[j].append(i)
-            return graph
+        '''
+        0 1 2 3 4
+        _ _ _ _ _
+
+        f(0, prev = None)
+        '''
+
+        adj_list = defaultdict(list)
+        visited = [False] * n
+
+        # build adj_list
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+
+        def contains_cycle(node, prev):
+            if visited[node]:
+                return True
+            visited[node] = True
+            for neighbor in adj_list[node]:
+                if neighbor != prev:
+                    if contains_cycle(neighbor, node):
+                        return True
+            return False
+
+        if contains_cycle(0, None):
+            return False
+        return sum(1 if val is True else 0 for val in visited) == n
         
-        graph = createGraph()
-        visited = set()
-        isValid = True
-        
-        def dfs(i, prev):
-            nonlocal isValid
-            if not isValid:
-                return
-            if i in visited:
-                isValid = False
-                return
-            visited.add(i)
-            for j in graph[i]:
-                if j != prev:
-                    dfs(j, i)
-                
-        dfs(0, None)
-        return isValid and len(visited) == n
-            
