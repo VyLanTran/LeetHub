62f2fb6fc74d8f64b790928bad6cc8aba41dee5e
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-11-24 18:58:52 -0500

    Time: 4 ms (72.83%), Space: 18.9 MB (83.9%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,26 +1,62 @@
+class UnionFind:
+    def __init__(self, size):
+        self.rank = [1] * size
+        self.root = [i for i in range(size)]
+    
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_y] > self.rank[root_x]:
+                self.root[root_x] = root_y
+            elif self.rank[root_y] < self.rank[root_x]:
+                self.root[root_y] = root_x
+            else:
+                self.root[root_y] = root_x
+                self.rank[root_x] += 1 
+
+
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # DFS
-        # create adj list
-        adj_list = defaultdict(list)
-        num_vertices = len(isConnected)
-        visited = set()
-        res = 0
+        size = len(isConnected)
+        uf = UnionFind(size)
 
-        for i in range(num_vertices):
-            for j in range(i + 1, num_vertices):
+        for i in range(size):
+            for j in range(i + 1, size):
                 if isConnected[i][j] == 1:
-                    adj_list[i].append(j)
-                    adj_list[j].append(i)
-        def dfs(i):
-            visited.add(i)
-            for j in adj_list[i]:
-                if j not in visited:
-                    dfs(j)
+                    uf.union(i, j)
+                    # print(f"Union {i} and {j}, root: {uf.root}, rank: {uf.rank}")
 
+        # print(uf.root)
+
+        '''
+         0,1,2,3
+        [1,0,0,1] 0
+        [0,1,1,0] 1
+        [0,1,1,1] 2
+        [1,0,1,1] 3
+
+        0 - 3
+            |
+        1 - 2
+                0, 1, 2, 3
+        root = [0, 1, 2, 3]
+        rank = [1, 1, 1, 1]
+
+        root(2) = 1
+        root(3) = 0 
+        '''
         
-        for i in range(num_vertices):
-            if i not in visited:
-                dfs(i)
-                res += 1
-        return res
+        roots = set()
+        for i in range(size):
+            roots.add(uf.find(i))
+        return len(roots)
+
