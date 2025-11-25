9edcaa20222f68acfc19763ed22ce582ae01b287
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2025-11-25 15:55:23 -0500

    Time: 3 ms (90.46%), Space: 19.1 MB (49.38%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,28 +1,36 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.root[root_x] = root_y
+            else:
+                self.root[root_y] = root_x
+                if self.rank[root_x] == self.rank[root_y]:
+                    self.rank[root_x] += 1
+    
+    def is_connected(self, x, y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+        res = n
+
+        for i, j in edges:
+            if not uf.is_connected(i, j):
+                uf.union(i, j)
+                res -= 1
         
-        def buildAdj():
-            adj = dict()
-            for i in range(n):
-                adj[i] = []
-            for edge in edges:
-                u, v = edge[0], edge[1]
-                adj[u].append(v)
-                adj[v].append(u)
-            return adj
-        
-        adj = buildAdj()
-        visited = set()
-        
-        def dfs(u):
-            visited.add(u)
-            for v in adj[u]:
-                if v not in visited:
-                    dfs(v)
-        res = 0
-        for i in range(n):
-            if i not in visited:
-                dfs(i)
-                res += 1
         return res
-            
