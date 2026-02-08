3e34080ba6fbd493d3db8f988c9fbaa1e05f0f82
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2026-02-08 15:58:19 -0500

    Time: 1 ms (77.15%), Space: 14.9 MB (18.81%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int comp = target - num;
+            if (map.contains(comp))
+                return {i, map[comp]};
+            map[num] = i;
+        }
+
+        return vector<int>();
+    }
+};
