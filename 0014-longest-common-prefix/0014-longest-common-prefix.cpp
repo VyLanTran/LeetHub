36c4b55ce092b3e12cab2263115578c9a0b22da4
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2026-02-08 16:29:01 -0500

    Time: 3 ms (10.7%), Space: 15.8 MB (5.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-
-    string merge(string left, string right) {
-        int i = 0;
-        while (i < min(left.size(), right.size()) && left[i] == right[i]) {
-            i += 1;
-        }
-        return left.substr(0, i);
+    string longestCommonPrefix(vector<string>& strs) {
+        return divide(strs, 0, strs.size() - 1);
     }
 
-    string divide(int l, int r, vector<string>& strs) {
+    string divide(vector<string>& strs, int l, int r) {
         if (l == r)
             return strs[l];
         int m = l + (r - l) / 2;
-        string left_prefix = divide(l, m, strs);
-        string right_prefix = divide(m + 1, r, strs);
+        string left_prefix = divide(strs, l, m);
+        string right_prefix = divide(strs, m + 1, r);
         return merge(left_prefix, right_prefix);
     }
 
-    string longestCommonPrefix(vector<string>& strs) {
-        return divide(0, strs.size() - 1, strs);
+    string merge(string l, string r) {
+        int i = 0;
+        while (i < min(l.size(), r.size()) && l[i] == r[i]) {
+            i++;
+        }
+        return l.substr(0, i);
     }
 };
